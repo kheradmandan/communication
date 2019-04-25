2d10b085c3a090a52b9b20108b429948d5efaaf4
morteza <raeisi@zoho.com>
2019-04-25 22:44:29 +0430

    Add 'user' data to 'auth' response.

--- server/src/api/users/auth.js
+++ server/src/api/users/auth.js
@@ -22,14 +22,16 @@ export default function signIn(req, res, next) {
             type: sequelize.QueryTypes.SELECT,
         })
         .then(user => {
-            if (!user || !user.length) {
+            if (!user || user.length !== 1) {
                 throw new OperationError()
                     .appendMessage('Provided authentication data is not valid.');
             }
 
-            const {uuid, email, nickname} = user;
-            return sign({uuid, email, nickname});
+            const {uuid, email, nickname, createdAt} = user[0];
+            return sign({uuid, email, nickname, createdAt})
+                .then((token) => ({type: 'jwt', token, user: {uuid, email, nickname}}));
+
         })
-        .then(token => response(req, res)({type: 'jwt', token}))
+        .then(response(req, res))
         .catch(next);
 }
