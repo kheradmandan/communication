a268415769e22546564bd1319e09ecd8d67f3f88
morteza <raeisi@zoho.com>
2019-04-26 00:58:53 +0430

    Implement 'sterilization' middleware to handle inner errors.  - instanceOf CommunicationBaseError to be true

---
+++ server/src/middlewares/sterilization.js
@@ -0,0 +1,22 @@
+import CommunicationBaseError from '../errors/CommunicationBaseError';
+import response from '../core/response';
+
+/**
+ * Handle api owned errors
+ * @param err
+ * @param req
+ * @param res
+ * @param next
+ */
+
+export default function (err, req, res, next) {
+    if (err instanceof CommunicationBaseError) {
+        const payload = {
+            requestedOrigin: req.url,
+            message: err.message,
+            appendices: err.appendices,
+        };
+        return response(req, res, {type: 'error', status: err.code})(payload);
+    }
+    next();
+}
