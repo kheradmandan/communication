3bfd75e3ef1cf53644fc188d529c3c5332a2093c
morteza <raeisi@zoho.com>
2019-04-26 13:42:51 +0430

    Set const uuid for make test easy

--- server/src/seeders/20190420172247-User.js
+++ server/src/seeders/20190420172247-User.js
@@ -4,6 +4,7 @@ module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('Users', [
             {
+                uuid: 'ce0d1090-396f-4d2b-9f51-ee6ef2051a3f',
                 email: 'raeisi@zoho.com',
                 password: Sequelize.fn('crypt', '123', "gen_salt('bf', 8)"),
                 nickname: 'Morteza',
@@ -11,6 +12,7 @@ module.exports = {
                 updatedAt: new Date('at Apr 20 2019 23:46:54 GMT+0430 (+0430)'),
             },
             {
+                uuid: '95b36189-d2d4-480d-9881-0eadc96f617f',
                 email: 'mrraeisi@outlook.com',
                 password: Sequelize.fn('crypt', '123', "gen_salt('bf', 8)"),
                 nickname: 'Admin',
--- server/src/seeders/20190420205349-Eras.js
+++ server/src/seeders/20190420205349-Eras.js
@@ -4,6 +4,7 @@ module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('Eras', [
             {
+                uuid: '26608d95-6ace-4a0d-8df3-4923fc6329bf',
                 originId: 13,
                 title: '1398 Contract',
                 createdAt: new Date(),
