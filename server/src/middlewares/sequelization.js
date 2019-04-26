c4ae7c02980011d015d306411c0297bff55e2aa0
morteza <raeisi@zoho.com>
2019-04-26 10:51:19 +0430

    Implement 'Sequelization' middleware to do collect ORM errors widely.  - it produces a CommunicationBaseError  - it does not send any response

---
+++ server/src/middlewares/sequelization.js
@@ -0,0 +1,26 @@
+import OperationError from "../errors/OperationError";
+
+/**
+ * Provides wide mechanism to handle Sequelize ORM errors
+ * @param err
+ * @param req
+ * @param res
+ * @param next
+ */
+
+export default function (err, req, res, next) {
+    if (/Sequelize.*Error/ig.test(err.name)) {
+        const productionMode = process.env['NODE_ENV'] === 'production';
+        const error = new OperationError();
+        if (productionMode) {
+            error
+                .appendMessage('Something goes wrong with database or does not meet validations');
+        } else {
+            error
+                .appendMessage(`[${err.name}] raised up`)
+                .appendMessage('origin: ' + err.message)
+        }
+        throw error;
+    }
+    next();
+}
