d8133b85d7b4a6252a2da9e46960260475543686
morteza <raeisi@zoho.com>
2019-04-28 22:14:24 +0430

    Rebuild create issue by assignee.

--- server/src/api/issues/create.js
+++ server/src/api/issues/create.js
@@ -1,19 +1,19 @@
-import {sequelize, Issue, User, Era} from '../../models';
+import {sequelize, Issue, User, Era, Assignee} from '../../models';
 import response from '../../core/response';
 import FieldMissingError from "../../errors/FieldMissingError";
 
 export default function (req, res, next) {
     const currentUser = req.user;
     const {
-        intentionTo,
+        userUuid = currentUser.uuid,
         eraUuid,
         priorityId = 0,
         statusId = 0,
         realmId,
         title,
     } = req.body;
-    if (!intentionTo || !eraUuid || !title || !realmId) {
-        throw new FieldMissingError().appendMessage(['intentionTo', 'eraUuid', 'realmId', 'title']);
+    if (!eraUuid || !title || !realmId) {
+        throw new FieldMissingError().appendMessage(['eraUuid', 'realmId', 'title']);
     }
 
     Promise.all(
@@ -22,33 +22,54 @@ export default function (req, res, next) {
                 include: ['Origin'],
                 rejectOnEmpty: true
             }),
-            User.findByPk(intentionTo, {
+            User.findByPk(userUuid, {
                 rejectOnEmpty: true
             }),
             User.findByPk(currentUser.uuid, {
                 rejectOnEmpty: true
             }),
         ])
-        .then(([era, intention, creator]) =>
-            sequelize.transaction(({transaction}) =>
-                era
-                    .increment('current', {by: era.getDataValue('increment')}, {transaction})
-                    .then((era) =>
-                        Issue
-                            .create({
-                                intentionTo: intention.uuid,
-                                createdBy: creator.uuid,
-                                eraUuid: era.uuid,
-                                sequence: era.current,
-                                priorityId,
-                                statusId,
-                                realmId,
-                                title
-                            }, {transaction})
-                    )
-            )
+        .then(([era, assignee, creator]) =>
+            sequelize
+                .transaction((transaction) =>
+                    era
+                        .increment('current', {by: era.getDataValue('increment')}, {transaction})
+                        .then((era) =>
+                            Issue
+                                .create({
+                                    createdBy: creator.uuid,
+                                    eraUuid: era.uuid,
+                                    sequence: era.current,
+                                    priorityId,
+                                    statusId,
+                                    realmId,
+                                    title
+                                }, {transaction})
+                        )
+                        .then((issue) =>
+                            Assignee
+                                .create({
+                                    issueUuid: issue.uuid,
+                                    userUuid: assignee.uuid,
+                                    createdBy: creator.uuid
+                                }, {transaction})
+                        )
+                        .then(assignee =>
+                            assignee
+                                .setParent(assignee, {transaction})
+                        )
+                )
         )
-        .then(issue => issue.get())
+        .then(assignee =>
+            Issue
+                .findAll({
+                    where: {uuid: assignee.issueUuid},
+                    include: [
+                        {model: User, as: 'Creator'},
+                        {model: Assignee},
+                    ]
+                })
+        )
         .then(response(req, res))
         .catch(next)
 }
