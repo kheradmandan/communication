0816bc6a1028a5363df0e1fce26bc4ed6d0721d6
morteza <raeisi@zoho.com>
2019-05-02 18:15:30 +0430

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	react-client/src/App.js
    	react-client/src/components/Dashboard/index.js
    	react-client/src/index.js

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-import App from './App';
-import SignIn from './components/SignIn';
-import * as serviceWorker from './serviceWorker';
-
-import 'semantic-ui-css/semantic.min.css'
-
+import {BrowserRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
 import applicationStore from './stores/application';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+
+import 'semantic-ui-css/semantic.min.css'
+import './index.css';
 
 const store = applicationStore();
 
 function main(Component) {
     ReactDOM.render(
         <Provider store={store}>
-            <Component/>
+            <BrowserRouter>
+                <Component/>
+            </BrowserRouter>
         </Provider>
         , document.getElementById('root'));
 }
 
-// main(App);
-main(SignIn);
+main(App);
+// main(SignIn);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
