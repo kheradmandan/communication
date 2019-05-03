3e57fc7fea1f48018903eb670d0bf800ac26bd97
morteza <raeisi@zoho.com>
2019-05-03 20:48:44 +0430

    First try of Comments implementation.

--- react-client/src/components/Issue/Issue.js
+++ react-client/src/components/Issue/Issue.js
@@ -4,6 +4,7 @@ import propTypes from 'prop-types';
 import {Map} from "immutable";
 import * as issueActions from '../../actions/issues';
 import IssueMainData from "../IssueMainData";
+import {Segment, Grid, Divider, Tab, Menu, Label, Icon, Comment, Form, TextArea, Button} from "semantic-ui-react";
 
 class Issue extends React.Component {
     state = {uuid: ''};
@@ -23,6 +24,17 @@ class Issue extends React.Component {
 
         return <div>
             <IssueMainData issue={current}/>
+            <Segment>
+                <Grid columns={2} stackable relaxed='very'>
+                    <Grid.Column>
+                        <CommentPane comments={current.get('Comments')}/>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <LeftPanel/>
+                    </Grid.Column>
+                </Grid>
+                <Divider vertical>*</Divider>
+            </Segment>
         </div>
     }
 }
@@ -43,3 +55,38 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps, issueActions)(Issue);
+
+const panes = [
+    {
+        menuItem: {key: 'histories', icon: 'history', content: 'تاریخچه'},
+        render: () => <Tab.Pane>histories go here</Tab.Pane>
+    },
+    {
+        menuItem: (<Menu.Item key='attachments'>
+            <Icon name='attach'/>
+            پیوست ها
+            <Label>0</Label>
+        </Menu.Item>),
+        render: () => <Tab.Pane>attach go here</Tab.Pane>
+    },
+];
+const LeftPanel = () => <Tab panes={panes}/>;
+
+function CommentPane({comments}) {
+    return (<Comment.Group>
+        {
+            comments && comments.map(comment =>
+                <Comment>
+                    <Comment.Content>
+                        Comment 1
+                    </Comment.Content>
+                </Comment>)
+        }
+        <Form>
+            <TextArea/>
+            <Button>
+                Submit
+            </Button>
+        </Form>
+    </Comment.Group>)
+}
