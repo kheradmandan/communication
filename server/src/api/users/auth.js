028c0b7b6971b13ec63feb7314f1ff2b41d3fdda
morteza <raeisi@zoho.com>
2019-05-16 14:11:29 +0430

    Async auth user. improve & test.

--- server/src/api/users/auth.js
+++ server/src/api/users/auth.js
@@ -1,17 +1,17 @@
 import {sequelize, User} from '../../models'
-import {sign} from "../../core/tokenizator";
 import FieldMissingError from "../../errors/FieldMissingError";
 import OperationError from "../../errors/OperationError";
-import response from '../../core/response';
+import {safeAsync} from "../../core/safe-async-middleware";
+import {sign} from "../../core/tokenizator";
 
-export default function signIn(req, res, next) {
-
+export default safeAsync(async function (req, res, next) {
     const {email, password} = req.body;
     if (!email || !password) {
         throw new FieldMissingError().appendMessage(['email', 'password']);
     }
 
-    sequelize.query(
+    // validate
+    const users = await sequelize.query(
         `SELECT "uuid", "email", "fullName", "userTypeId"
                      FROM "Users" 
                      WHERE "deletedAt" IS NULL AND "email" = :email AND "password" = crypt(:password,"password")
@@ -20,30 +20,37 @@ export default function signIn(req, res, next) {
             model: User,
             replacements: {email, password},
             type: sequelize.QueryTypes.SELECT,
-        })
-        .then(users => {
-            if (!users || users.length !== 1) {
-                throw new OperationError()
-                    .appendMessage('Provided authentication data is not valid.');
-            }
+        });
 
-            const user = users[0];
-            return Promise.all([user.get(), user.getUserType()]);
-        })
-        .then(([user, userType]) => {
-            const {uuid, email, fullName, userTypeId, createdAt} = user;
-            return sign({uuid, email, fullName, userTypeId, createdAt})
-                .then((token) => ({
-                    type: 'jwt',
-                    token,
-                    user: {
-                        uuid,
-                        email,
-                        fullName,
-                        userType: userType
-                    }
-                }));
-        })
-        .then(response(req, res))
-        .catch(next);
-}
+    // failed
+    if (!users || users.length !== 1) {
+        throw new OperationError().appendMessage('Provided authentication data is not valid.');
+    }
+
+    // get persist
+    const validatedUser = users[0];
+    const [user, userType] = await Promise.all([
+        validatedUser.get(),
+        validatedUser.getUserType()
+    ]);
+
+    // token
+    async function generatePayload(user, userType) {
+        const {uuid, email, fullName, userTypeId, createdAt} = user;
+        const token = await sign({uuid, email, fullName, userTypeId, createdAt});
+        return {
+            type: 'jwt',
+            token,
+            user: {
+                uuid,
+                email,
+                fullName,
+                userType,
+            }
+        };
+    }
+
+    // passed
+    res.locals.payload = await generatePayload(user, userType);
+    next();
+})
