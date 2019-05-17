aca5a19e3e789965e17c1137984d4d068120ed87
morteza <raeisi@zoho.com>
2019-05-17 21:28:05 +0430

    Save state and use <RealmSelection> component.  - it works and I appreciate REACT & Dan Ambrov.

--- react-client/src/components/IssueDialog/IssueDialog.js
+++ react-client/src/components/IssueDialog/IssueDialog.js
@@ -1,37 +1,82 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {Button, Form, Input, Modal, TextArea} from "semantic-ui-react";
+import {Button, Form, Input, Label, Modal, TextArea} from "semantic-ui-react";
 import Selection from "../Selection";
+import RealmSelection from "../RealmSelection";
 
 class IssueDialog extends React.Component {
 
+    state = {
+        Origin: 0,
+        Realm: 0,
+        Priority: 0,
+    };
+
+    handleSelectionChange = (e, {name, value}) => {
+        const newState = {[name]: value};
+        if (name === 'Origin') {
+            newState.Realm = 0;
+        }
+        this.setState(newState);
+    };
+
     render() {
         const {permissions} = this.props;
+        const {Origin, Realm, Priority} = this.state;
+
         return (<Modal trigger={<Button>Add new </Button>} centered={false}>
             <Modal.Header>
                 اعلام مشکل
             </Modal.Header>
             <Modal.Content>
                 <Form>
-                    <Input
-                        fluid
-                        placeholder="title"
-                        icon="bug"
-                    />
-                    <TextArea placeholder="exp"/>
-
-                    <Modal.Content>
-                        <Selection options={permissions.get('xref-users-origins')} modelName='Origin'/>
-                    </Modal.Content>
-
-                    <Modal.Actions>
-                        <Button primary>
-                            ذخیره
-                        </Button>
-                        <Button secondary>
-                            انصراف
-                        </Button>
-                    </Modal.Actions>
+                    <Form.Field>
+                        <Label>عنوان*</Label>
+                        <Input
+                            fluid
+                            placeholder="عنوان مختصر*"
+                            icon="bug"
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <Label>شرح</Label>
+                        <TextArea placeholder="شرح مسأله"/>
+                    </Form.Field>
+                    <Form.Group>
+                        <Form.Field>
+                            <Label>محل</Label>
+                            <Selection defaultValue={Origin}
+                                       options={permissions.get('xref-users-origins')}
+                                       modelName='Origin'
+                                       onSelection={this.handleSelectionChange}
+                            />
+                        </Form.Field>
+                        <Form.Field>
+                            <Label>بخش</Label>
+                            <RealmSelection defaultValue={Realm}
+                                            originId={Origin}
+                                            onSelection={this.handleSelectionChange}
+                            />
+                        </Form.Field>
+                        <Form.Field>
+                            <Label>اولویت</Label>
+                            <Selection defaultValue={Priority}
+                                       options={permissions.get('xref-users-origins')}
+                                       modelName='Priority'
+                                       onSelection={this.handleSelectionChange}
+                            />
+                        </Form.Field>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Field>
+                            <Button primary>
+                                ذخیره
+                            </Button>
+                            <Button secondary>
+                                انصراف
+                            </Button>
+                        </Form.Field>
+                    </Form.Group>
                 </Form>
             </Modal.Content>
 
