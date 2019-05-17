8904759cd997c898d9dffdbf2b6897c23e61a4f7
morteza <raeisi@zoho.com>
2019-05-17 21:26:38 +0430

    Pass selected value to parent component.

--- react-client/src/components/Selection/Selection.js
+++ react-client/src/components/Selection/Selection.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import {Dropdown} from "semantic-ui-react";
 import {List} from "immutable";
+import {Dropdown} from "semantic-ui-react";
 
-
-export default function ({options, modelName}) {
+export default function ({options, modelName, onSelection, defaultValue}) {
 
     if (!options) options = [];
     if (List.isList(options)) options = options.toJS();
 
     let mappedOptions = options
-        .filter(x => x[modelName]['parentId'])
+        .filter(x => x[modelName] && x[modelName]['parentId'])
         .map(x => ({key: x[modelName]['id'], value: x[modelName]['id'], text: x[modelName]['title']}));
 
     return <Dropdown
         openOnFocus
         selection
         options={mappedOptions}
+        value={defaultValue}
+        name={modelName}
+        onChange={onSelection}
     />
 
 }
