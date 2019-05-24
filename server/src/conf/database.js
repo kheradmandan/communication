921aa861f7469e25849a2004e8eeb04b98fcf75c
morteza <raeisi@zoho.com>
2019-05-24 20:43:00 +0430

    Change Sequelize config file and type.

---
+++ server/src/conf/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "development": {
+        "use_env_variable": "_COMMUNICATION_CON",
+        "logging": false
+    },
+    "test": {
+        "use_env_variable": "_COMMUNICATION_CON"
+    },
+    "production": {
+        "use_env_variable": "_COMMUNICATION_CON"
+    }
+};
