e1555e5c546089bdeac897b4fd43a231c0e7c4be
morteza <raeisi@zoho.com>
2019-06-05 22:31:36 +0430

    Wisp Sequelize and switch to Mongoose.

--- server/src/api/auth/auth.js
+++ server/src/api/auth/auth.js
@@ -1,7 +1,7 @@
-const {sequelize, User} = require('../../models');
-const {sign} = require('../../core/tokenizator');
 const Joi = require('@hapi/joi');
 const Boom = require('@hapi/boom');
+const User = require('../../schemas/user');
+const {sign} = require('../../core/tokenizator');
 
 module.exports.validate = {
     payload: Joi.object({
@@ -14,44 +14,40 @@ module.exports.handler = async function (request, h) {
 
     // validate
     const {email, password} = request.payload;
-    const users = await sequelize.query(
-        `SELECT "uuid", "email", "fullName", "userTypeId"
-                     FROM "Users" 
-                     WHERE "deletedAt" IS NULL AND "email" = :email AND "password" = crypt(:password,"password")
-                     LIMIT 1;`,
-        {
-            model: User,
-            replacements: {email, password},
-            type: sequelize.QueryTypes.SELECT,
-        });
+    const user = await User.findOne({email});
 
     // failed
-    if (!users || users.length !== 1) {
+    if (!user) {
         throw Boom.unauthorized('Provided authentication data is not valid.');
     }
 
     // get persist
-    const validatedUser = users[0];
-    const [user, userType] = await Promise.all([
-        validatedUser.get(),
-        validatedUser.getUserType()
-    ]);
-
-    // token
-    async function generatePayload(user, userType) {
-        const {uuid, email, fullName, userTypeId, createdAt} = user;
-        const token = await sign({uuid, email, fullName, userTypeId, createdAt});
-        return {
-            type: 'jwt',
-            token,
-            user: {
-                uuid,
-                email,
-                fullName,
-                userType,
-            }
-        };
+    const match = await user.comparePassword(password);
+    if (match !== true) {
+        throw Boom.unauthorized('Provided authentication data is not valid.');
     }
 
-    return generatePayload(user, userType);
+    // explicitly define
+    const credential = {
+        _id: user._id,
+        name: user.name,
+        family: user.family,
+        email: user.email,
+        group: user.group,
+    };
+
+    // token
+    const token = await sign(credential);
+    return {
+        type: 'jwt',
+        token,
+        user: {
+            // explicitly again
+            _id: user._id,
+            name: user.name,
+            family: user.family,
+            email: user.email,
+            group: user.group,
+        }
+    };
 };
