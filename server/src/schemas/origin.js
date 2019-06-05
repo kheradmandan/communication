6b6852f92d5b6ea24cfbff4c0b61c943f32fc3d9
morteza <raeisi@zoho.com>
2019-06-05 23:30:34 +0430

    Define Origin schema indices.

--- server/src/schemas/origin.js
+++ server/src/schemas/origin.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 const OriginSchema = new mongoose.Schema({
     title: {
         type: String,
+        unique: true,
         required: true,
+        dropDups: true,
         minLength: 2,
         maxLength: 32,
-        index: true,
     }
 });
 
-OriginSchema.index({title: 1});
+OriginSchema.index({title: 1}, {unique: true});
 
 module.exports = mongoose.model('Origin', OriginSchema);
