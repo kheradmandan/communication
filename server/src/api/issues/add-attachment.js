390e229ff41865e9c05efe69474aa4f20d761cf4
morteza <raeisi@zoho.com>
2019-06-06 22:25:21 +0430

    Add attachment as a dependency and remove from issue.

--- server/src/api/issues/add-attachment.js
+++ server/src/api/issues/add-attachment.js
@@ -1,5 +1,6 @@
 const Boom = require('@hapi/boom');
 const Issue = require('../../schemas/issue');
+const Attachment = require('../../schemas/attachment');
 
 module.exports.validate = {};
 
@@ -8,7 +9,16 @@ module.exports.handler = async function (request) {
     const issueId = request.params.id;
     const {File, title} = request.payload;
 
-    const newAttachment = {
+    const issue = await Issue.findById(issueId).exec();
+    if (!issue) {
+        throw Boom.badData('Specified issue not found. Maybe you have not enough permission.');
+    }
+
+    const newAttachment = new Attachment({
+        owner: {
+            id: issue._id,
+            name: 'Issue'
+        },
         title,
         filename: File.hapi.filename,
         data: new Buffer(File._data),
@@ -16,23 +26,15 @@ module.exports.handler = async function (request) {
             by: currentUser._id,
             at: new Date()
         }
-    };
-
-    const issue = await Issue.findById(issueId).exec();
-    if (!issue) {
-        throw Boom.badData('Specified issue not found. Maybe you have not enough permission.');
-    }
-
+    });
     // save comment at first position
-    issue.attachments.unshift(newAttachment);
-    await issue.save();
+    await newAttachment.save();
 
-    // return issue.attachments[0];
-    const justCreated = issue.attachments[0];
     return {
-        _id: justCreated._id,
-        title: justCreated.title,
-        filename: justCreated.filename,
+        _id: newAttachment._id,
+        owner: newAttachment.owner,
+        title: newAttachment.title,
+        filename: newAttachment.filename,
         data: 'accepted'
     }
 };
