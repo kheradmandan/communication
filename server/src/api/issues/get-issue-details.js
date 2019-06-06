8c6c5a6ce3d6577d9ecb552be53f22100b5263cb
morteza <raeisi@zoho.com>
2019-06-06 19:06:39 +0430

    Separate and refactor and add validations.

---
+++ server/src/api/issues/get-issue-details.js
@@ -0,0 +1,25 @@
+const Joi = require('@hapi/joi');
+const Boom = require('@hapi/boom');
+const Issue = require('../../schemas/issue');
+const CONSTANTS = require('../../core/constants');
+
+module.exports.validate = {
+    params: Joi.object({
+        id: CONSTANTS.joi.objectId(Joi).required()
+    })
+};
+
+module.exports.handler = async function (request) {
+    const currentUser = request.auth.credentials;
+    const issueId = request.params.id;
+
+    const issue = await Issue
+        .findOne({_id: issueId, "created.by": currentUser._id})
+        .populate('created.by', 'name family')
+    ;
+    if (!issue) {
+        throw Boom.badData('Specified issue not found. Maybe you have not enough permission.');
+    }
+
+    return issue;
+};
