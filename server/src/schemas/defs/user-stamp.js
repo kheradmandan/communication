0434d5d49b9c8ed3e36d87655ce8bfb8caf146f9
morteza <raeisi@zoho.com>
2019-06-06 10:53:55 +0430

    Implement and define.

---
+++ server/src/schemas/defs/user-stamp.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const UserStampSchema = new mongoose.Schema({
+    by: {
+        type: mongoose.Schema.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+    at: {
+        type: Date,
+        required: true,
+        default: new Date(),
+    }
+});
+
+module.exports = UserStampSchema;
