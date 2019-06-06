1eacf1ac70a9b647b91c73806dced22bf2562ad5
morteza <raeisi@zoho.com>
2019-06-06 11:37:52 +0430

    Implementation.

---
+++ server/src/schemas/realm.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const RealmSchema = new mongoose.Schema({
+    _id: {
+        type: Number,
+    },
+    parent: {
+        type: Number,
+        ref: 'Realm'
+    },
+    title: {
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 32,
+    }
+});
+
+RealmSchema.index({title: 1});
+
+module.exports = mongoose.model('Realm', RealmSchema);
