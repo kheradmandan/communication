64e11980c2747b7b7add9438e6ab5a1a1a6909c5
morteza <raeisi@zoho.com>
2019-06-07 00:04:56 +0430

    Remove static routes. {{ Farewell }}

--- server/src/api/statics/index.js
+++
@@ -1,6 +0,0 @@
-import protection from "../../middlewares/protection";
-import readComboItems from './read-combo-items';
-
-export default function (router) {
-    router.get('/statics', protection, readComboItems);
-}
