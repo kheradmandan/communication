2355ff5bfe55df975ca9ba5b27394a5426bb7b0c
morteza <raeisi@zoho.com>
2019-06-07 00:02:55 +0430

    Remove my express tools. {{ Farewell }}

--- server/src/core/safe-async.js
+++
@@ -1,9 +0,0 @@
-/**
- * Safe Async Middleware
- * @param asyncMiddleware
- * @returns {function(*=, *=, *=): Promise<any | never>}
- */
-export const safeAsync = asyncMiddleware => (req, res, next) =>
-    Promise
-        .resolve(asyncMiddleware(req, res, next))
-        .catch(next);
