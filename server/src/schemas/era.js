b1c03243a0e1a13a223f0e54df8e5a909b372083
morteza <raeisi@zoho.com>
2019-06-06 16:08:51 +0430

    Rebuild get next sequence.

--- server/src/schemas/era.js
+++ server/src/schemas/era.js
@@ -21,23 +21,11 @@ const EraSchema = new mongoose.Schema({
 
 EraSchema.index({origin: 1, title: 1}, {unique: true});
 
-EraSchema.method.getNextSequence = async function getNextSequence() {
+EraSchema.methods.getNextSequence = async function getNextSequence() {
     // Race condition!!!
-    let threshold = 10;
-    while (--threshold) {
-        const current = await EraSchema.findOne({_id: this._id});
-        const {nModified} = await EraSchema.updateOne({
-            _id: current._id,
-            seq: current.seq
-        }, {
-            $inc: {seq: current.inc}
-        });
-        if (nModified) {
-            const successIncrement = await EraSchema.findOne({_id: this._id});
-            return successIncrement.seq;
-        }
-    }
-    throw new Error('Cannot getNextSequence because threshold reached.')
+    this.seq += this.inc;
+    await this.save();
+    return this.seq;
 };
 
 module.exports = mongoose.model('Era', EraSchema);
