bba2da845363e634016facbd285432239e78a9dc
morteza <raeisi@zoho.com>
2019-06-06 14:41:13 +0430

    Switch to Hapi & Mongo.

--- server/src/api/issues/create.js
+++ server/src/api/issues/create.js
@@ -1,79 +1,62 @@
-import {sequelize, Issue, User, Era, Assignee} from '../../models';
-import FieldMissingError from "../../errors/FieldMissingError";
-import {safeAsync} from "../../core/safe-async";
+const Joi = require('@hapi/joi');
+const Boom = require('@hapi/boom');
+const Era = require('../../schemas/era');
+const User = require('../../schemas/user');
+const Issue = require('../../schemas/issue');
+const CONSTANTS = require('../../core/constants');
 
-export default safeAsync(async function (req, res, next) {
-    const currentUser = req.user;
+module.exports.validate = {
+    payload: Joi.object({
+        era: CONSTANTS.joi.objectId(Joi).required(),
+        title: Joi
+            .string()
+            .min(CONSTANTS.mongo.issue.title.minLength)
+            .max(CONSTANTS.mongo.issue.title.maxLength)
+            .required(),
+        realm: Joi.number().required(),
+        status: Joi.string().empty('').default('draft').valid(CONSTANTS.mongo.issue.statuses),
+        priority: Joi.string().empty('').default('normal').valid(CONSTANTS.mongo.issue.priorities),
+    })
+};
+
+module.exports.handler = async function (request, h) {
+
+    const currentUser = request.auth.credentials;
     const {
-        userUuid = currentUser.uuid,
-        eraUuid,
-        viewpointId = 0,
-        priorityId = 0,
-        statusId = 0,
-        realmId,
+        user = currentUser._id,
+        era,
+        priority,
+        status,
+        realm,
         title,
-    } = req.body;
-    if (!eraUuid || !title || !realmId) {
-        throw new FieldMissingError().appendMessage(['eraUuid', 'realmId', 'title']);
-    }
+    } = request.payload;
 
     // insurance
-    const rejectOnEmpty = true;
-    let [era, assigneeUserCandidate, creator] = await Promise.all([
-        Era.findByPk(eraUuid, {rejectOnEmpty}),
-        User.findByPk(userUuid, {rejectOnEmpty}),
-        User.findByPk(currentUser.uuid, {rejectOnEmpty}),
-    ]);
-
-    let transaction;
-    try {
-        // start transaction
-        transaction = await sequelize.transaction();
-
-        // generate new sequence number
-        era = await era.increment('current', {by: era.getDataValue('increment')}, {transaction});
-
-        // save issue
-        const issue = await Issue.create({
-            createdBy: creator.uuid,
-            eraUuid: era.uuid,
-            sequence: era.current,
-            priorityId,
-            statusId,
-            realmId,
-            title
-        }, {transaction});
-
-        // create default assignee
-        let assignee = await Assignee.create({
-            issueUuid: issue.uuid,
-            userUuid: assigneeUserCandidate.uuid,
-            createdBy: creator.uuid,
-            viewpointId
-        }, {transaction});
-
-        // self reference for head assignee
-        assignee = await assignee.setParent(assignee, {transaction});
-
-        // fetch created issue
-        const createdIssue = await Issue
-            .findAll({
-                where: {uuid: assignee.issueUuid},
-                include: [
-                    {model: Assignee},
-                ],
-                transaction
-            });
-
-        // commit changes
-        await transaction.commit();
-
-        // passed
-        res.locals.payload = createdIssue;
-        next();
-
-    } catch (e) {
-        transaction && await transaction.rollback();
-        throw e;
+    const userInstance = await User.findById(user, '_id');
+    if (!userInstance) {
+        throw Boom.badData('Specified user is not valid');
     }
-})
+
+    const eraInstance = await Era.findById(era);
+    if (!eraInstance) {
+        throw Boom.badData('Specified era is not valid');
+    }
+
+    // make sure all date are same
+    const currentDate = new Date();
+    const created = (id = userInstance._id) => ({by: id, at: currentDate});
+
+    const issue = new Issue({
+        title,
+        realm,
+        era,
+        sequence: eraInstance.getNextSequence(),
+        statuses: [{id: status, created: created()}],
+        priorities: [{id: priority, created: created()}],
+        assignees: [{user: userInstance._id, created: created()}],
+        created: created(),
+    });
+    await issue.save();
+
+    return issue;
+};
