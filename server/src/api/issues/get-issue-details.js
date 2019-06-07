3ee6b865da17ea6e196fe6be54a471beedeaa214
morteza <raeisi@zoho.com>
2019-06-07 16:13:44 +0430

    Check role (simplified and hard code). Access Control Service is under implementation.

--- server/src/api/issues/get-issue-details.js
+++ server/src/api/issues/get-issue-details.js
@@ -13,13 +13,13 @@ module.exports.handler = async function (request) {
     const currentUser = request.auth.credentials;
     const issueId = request.params.id;
 
-    const issue = await Issue
-        .findOne({_id: issueId, "created.by": currentUser._id})
-        .populate('created.by', 'name family')
-    ;
-    if (!issue) {
-        throw Boom.badData('Specified issue not found. Maybe you have not enough permission.');
+    const role = await Issue.getRole(issueId, currentUser._id);
+    console.log('your role is ', role);
+    if (!['CREATOR', 'ASSIGNEE', 'ASSIGNED'].some(x => x === role)) {
+        throw Boom.badData('Specified issue not found. Maybe you have not enough permission. ' + role);
     }
 
-    return issue;
+    return Issue
+        .findById(issueId)
+        .populate('created.by', 'name');
 };
