635834c63efe4159d5fb57e2dd3c6532e5becf94
morteza <raeisi@zoho.com>
2019-06-07 16:30:36 +0430

    Use 'mongoose.create' to do pre save actions.

--- server/src/models/seeds/user-seed.js
+++ server/src/models/seeds/user-seed.js
@@ -3,41 +3,26 @@ const User = require('../user');
 module.exports = async function userSeed(mongoose) {
 
     await mongoose.connection.db.dropCollection('users');
-    await User.insertMany([
-        {
-            _id: '5cf80118b2b7b13c6dfe9f00',
-            name: {
-                gender: 'آقای',
-                first: 'مرتضی',
-                last: 'رییسی',
-            },
-            email: 'raeisi@zoho.com',
-            group: {id: 8, title: 'برنامه نویس محاسب'},
-            password: '123',
+    await User.create({
+        _id: '5cf80118b2b7b13c6dfe9f00',
+        name: {
+            gender: 'آقای',
+            first: 'مرتضی',
+            last: 'رییسی',
         },
-        {
-            _id: '5cf80118b2b7b13c6dfe9f01',
-            name: {
-                gender: 'آقای',
-                first: 'داود',
-                last: 'محمدی',
-            },
-            email: 'd.mohammadi.a@gmail.com',
-            group: {id: 5, title: 'کارشناس مقیم'},
-            password: '123',
+        email: 'raeisi@zoho.com',
+        group: {id: 8, title: 'برنامه نویس محاسب'},
+        password: '123',
+    });
+    await User.create({
+        _id: '5cf80118b2b7b13c6dfe9f01',
+        name: {
+            gender: 'آقای',
+            first: 'داود',
+            last: 'محمدی',
         },
-    ]);
-    const user = await User.findOne({email: 'raeisi@zoho.com'});
-    if (!user) {
-        const newUser = new User({
-            _id: '5cf80118b2b7b13c6dfe9f00',
-            name: 'مرتضی',
-            family: 'رییسی',
-            email: 'raeisi@zoho.com',
-            group: {id: 8, title: 'برنامه نویس محاسب'},
-            password: '123',
-        });
-        await newUser.save();
-        console.log('root user created.');
-    }
+        email: 'd.mohammadi.a@gmail.com',
+        group: {id: 5, title: 'کارشناس مقیم'},
+        password: '123',
+    });
 };
