5cb4513790e13b314d359fb2ecfc4edf5373635b
morteza <raeisi@zoho.com>
2019-06-07 18:42:42 +0430

    Populate and select necessary fields.

--- server/src/api/issues/get-issue-list.js
+++ server/src/api/issues/get-issue-list.js
@@ -12,6 +12,23 @@ module.exports.handler = async function (request) {
     const {limit} = request.query;
 
     return Issue
-        .find({"assignees.0.user": currentUser._id})
+        .find({"assignees.0.user": currentUser._id}, {
+            assignees: {$slice: 1},
+            statuses: {$slice: 1},
+            priorities: {$slice: 1}
+        })
+        .select('title realm era statuses.id priorities.id sequence created')
+        .populate('created.by', 'name')
+        .populate('assignees.0.user', 'name')
+        .populate('assignees.0.created.by', 'name')
+        .populate('realm', 'title')
+        .populate({
+            path: 'era',
+            select: 'title',
+            populate: {
+                path: 'origin',
+                select: 'title'
+            }
+        })
         .limit(limit);
 };
