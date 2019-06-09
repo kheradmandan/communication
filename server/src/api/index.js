425d9c40893cf52b740205f4c303599804cfdc82
morteza <raeisi@zoho.com>
2019-06-09 21:18:53 +0430

    Register routes automatically.

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,25 +1,39 @@
-const auth = require('./auth');
-const issues = require('./issues');
-//const users = require('./users');
-// import statics from './statics';
-// import permissions from './permissions';
-//
-// const router = express.Router();
-//
-// // register routes
-// users(router);
-// issues(router);
-// statics(router);
-// permissions(router);
-//
-// export default router;
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Find Controllers and register their routes
+ * @param server
+ * @param option
+ * @returns {Promise<void>}
+ */
 async function registerRoutes(server, option) {
 
-    auth(server, option);
-    issues(server, option);
+    fs
+        .readdirSync(__dirname)
+
+        // Pull directories only
+        .filter(fileOrDir => fileOrDir.indexOf('.') === -1)
+
+        // Directory contents
+        .forEach(directory => {
+            fs
+                .readdirSync(path.resolve(__dirname, directory))
+
+                // Filter default paths
+                .filter(filename => filename !== 'index.js')
+
+                // Register one by one
+                .forEach(filename => {
+
+                    const route = require(path.resolve(__dirname, directory, filename));
+                    route(server, option)
+
+                });
+        });
 }
 
 exports.plugin = {
-    name: 'registerRoutes',
+    ...require('./package'),
     register: registerRoutes,
 };
