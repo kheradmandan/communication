ba357b1be00b57a32d848cfc7ffe6e894f2daeb1
morteza <raeisi@zoho.com>
2019-06-10 20:23:15 +0430

    Switch to Immutable.Js.

--- react-client/src/components/User/User.js
+++ react-client/src/components/User/User.js
@@ -4,7 +4,8 @@ export default function ({source}) {
     if (!source) {
         return <p>- no - user - </p>
     }
-
-    let {gender, first, last} = source.name;
+    const gender = source.getIn(['name', 'gender']);
+    const first = source.getIn(['name', 'first']);
+    const last = source.getIn(['name', 'last']);
     return <p> {`${gender} ${first} ${last}`}</p>
 }
