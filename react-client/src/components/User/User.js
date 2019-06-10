b48f4c95fee9daa6016c16fd04f0cec418bd0f9d
morteza <raeisi@zoho.com>
2019-06-10 21:05:24 +0430

    Add 'by' optional prop.

--- react-client/src/components/User/User.js
+++ react-client/src/components/User/User.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function ({source}) {
+export default function ({source, by = ''}) {
     if (!source) {
         return <p>- no - user - </p>
     }
     const gender = source.getIn(['name', 'gender']);
     const first = source.getIn(['name', 'first']);
     const last = source.getIn(['name', 'last']);
-    return <p> {`${gender} ${first} ${last}`}</p>
+    return <p> {`${by} ${gender} ${first} ${last}`}</p>
 }
