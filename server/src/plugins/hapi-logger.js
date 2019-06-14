fec6a5969612a0165d4be937d43600f01e4ca071
morteza <raeisi@zoho.com>
2019-06-14 17:46:08 +0430

    Log each request that made.

--- server/src/plugins/hapi-logger.js
+++ server/src/plugins/hapi-logger.js
@@ -8,18 +8,27 @@ const Boom = require('@hapi/boom');
  */
 async function myHapiLogger(server, option) {
 
+    server.events.on('response', function (request) {
+        request.log('info', request.response.statusCode);
+    });
+
     server.events.on('log', function (message) {
+
         console.error('server log: %o', message);
     });
 
     server.events.on('request', function (request, event, tags) {
-        const uri = `${new Date(event.timestamp).toUTCString()}: ${request.url.pathname} --> ${JSON.stringify(request.query)}`;
+        const uri = `[${new Date(event.timestamp).toUTCString()}] ${request.method.toUpperCase()} ${request.url.pathname} ${JSON.stringify(request.query)}`;
 
         if (Boom.isBoom(event.error)) {
             console.log('%s %s %o', uri, event.tags, event.error.output);
 
         } else {
-            console.log(uri, event, tags);
+            if (tags.info) {
+                console.log('%s --> %o',uri, event.data);
+            } else {
+                console.log(uri, event, tags);
+            }
         }
     });
 }
