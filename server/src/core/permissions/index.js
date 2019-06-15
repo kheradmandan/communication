4782702fe3caf663ae04524a279b3d19cd983c68
RaeisiM <raeisi@zoho.com>
2019-06-15 16:06:09 +0430

    Retrieve permission for origin.

--- server/src/core/permissions/index.js
+++ server/src/core/permissions/index.js
@@ -1,22 +1,57 @@
 const Issue = require('../../models/issue');
+const Origin = require('../../models/origin');
 
+/**
+ * Get all possible roles for issue
+ * @param userId
+ * @param issueId
+ * @returns {Promise<*[]>}
+ */
 module.exports.forIssue = async function getIssuePermissions(userId, issueId) {
 
     const roles = [];
-
-    function pushRoles(permissions) {
-        if (permissions && permissions.length > 0) {
-            roles.push(permissions.filter(x => x.key.toString() === userId))
-        }
-    }
-
     const issue = await Issue
         .findById(issueId)
-        .populate('era').select('permissions')
-        .populate('era.origin').select('permissions')
+        .populate('era').select('permissions origin')
         .exec();
-    pushRoles(issue.era.permissions);
-    pushRoles(issue.era.origin.permissions);
+    roles.push(filterRoles(userId, issue.era));
+
+    // extend from Origin
+    const originPermissions = await module.exports.forOrigin(userId, issueId.era.origin);
+
+    return [...new Set([...roles, ...originPermissions])]; // distinct
+};
+
+/**
+ * Get all possible roles for origin
+ * @param userId
+ * @param originId
+ * @returns {Promise<*[]>}
+ */
+module.exports.forOrigin = async function getOriginPermissions(userId, originId) {
+
+    const roles = [];
+
+    const origin = await Origin
+        .findById(originId)
+        .populate('parent').select('permissions')
+        .populate('parent.parent').select('permissions')
+        .exec();
+    roles.push(filterRoles(userId, origin));
+    roles.push(filterRoles(userId, origin.parent));
 
     return [...new Set(roles)]; // distinct
 };
+
+/**
+ * Helper to filter roles
+ * @param userId
+ * @param target
+ * @returns {T[]|Array}
+ */
+function filterRoles(userId, target) {
+    if (target && target.permissions && target.permissions.length > 0) {
+        return target.permissions.filter(x => x.key.toString() === userId);
+    }
+    return [];
+}
