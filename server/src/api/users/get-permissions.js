90ea30b1a66fc6084484b278dd26489bd5c58ea3
RaeisiM <raeisi@zoho.com>
2019-06-18 08:56:07 +0430

    Define and implement 'GET users/permissions' route.

---
+++ server/src/api/users/get-permissions.js
@@ -0,0 +1,26 @@
+const getAllPermissions = require('../../core/permissions/get-all');
+
+/**
+ * Controller for [GET users/permissions] route.
+ * @param server
+ * @param option
+ */
+
+module.exports = async function (server, option) {
+    server.route({
+        method: 'GET',
+        path: 'users/permissions',
+        handler,
+        options: {
+            validate
+        }
+    });
+};
+
+const validate = {};
+
+const handler = async function (request) {
+    const currentUser = request.auth.credentials;
+
+    return getAllPermissions(currentUser._id);
+};
