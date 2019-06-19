3beca0863e9b8d691d30fdbe99a6efff7e11d8bd
RaeisiM <raeisi@zoho.com>
2019-06-19 16:05:53 +0430

    Implement route for retrieve connections by era.

---
+++ server/src/api/permissions/get-connections.js
@@ -0,0 +1,34 @@
+const Joi = require('@hapi/joi');
+const connections = require('../../core/permissions/connections');
+const CONSTANTS = require('../../core/constants');
+
+/**
+ * Controller for [GET permissions/connections/{id}/era] route.
+ * @param server
+ * @param option
+ * @returns {Promise<void>}
+ */
+
+module.exports = async function (server) {
+    server.route({
+        method: 'GET',
+        path: 'permissions/connections/{id}/era',
+        handler,
+        options: {
+            validate
+        }
+    });
+};
+
+const validate = Joi.object({
+    params: {
+        id: CONSTANTS.joi.objectId(Joi).required(),
+    }
+});
+
+const handler = async function (request) {
+    const currentUser = request.auth.credentials;
+    const eraId = request.params.id;
+
+    return connections.forEra(currentUser._id, eraId);
+};
