8d7cecf58b4bea8330a0be27fef66a69bba87258
morteza <raeisi@zoho.com>
2019-06-20 19:48:16 +0430

    Update seeds.

--- server/src/models/seeds/era-seed.js
+++ server/src/models/seeds/era-seed.js
@@ -7,7 +7,23 @@ module.exports = async function originSeed(mongoose) {
         {_id: '5cf8efd21c3dc855e58756cd', origin: 2, originAncestors: [2, 1], title: 'سال ۱۳۹۸',},
         {_id: '5cf8efd21c3dc855e58756cc', origin: 3, originAncestors: [3, 1], title: 'سال ۱۳۹۸', inc: 10},
         {_id: '5cf8efd21c3dc855e58756ac', origin: 7, originAncestors: [7], title: 'سال ۱۳۹۸', inc: 2},
-        {_id: '5cf8efd21c3dc855e58756ab', origin: 9, originAncestors: [7], title: '1398'},
+        {
+            _id: '5cf8efd21c3dc855e58756ab', origin: 9, originAncestors: [7], title: '1398',
+            permissions: [
+                {
+                    user: '5cf80118b2b7b13c6dfe9f00',
+                    roles: ['add-issue', 'view-issue', 'add-comment'],
+                    connections: [{user: '5cf80118b2b7b13c6dfe9f01'}, {user: '5cf80118b2b7b13c6dfe9f02'},]
+                }, {
+                    user: '5cf80118b2b7b13c6dfe9f01',
+                    roles: ['add-issue', 'view-issue', 'add-comment'],
+                    connections: [{user: '5cf80118b2b7b13c6dfe9f00'}]
+                }, {
+                    user: '5cf80118b2b7b13c6dfe9f02',
+                    roles: ['add-issue', 'view-issue', 'add-comment'],
+                    connections: [{user: '5cf80118b2b7b13c6dfe9f01'}]
+                },]
+        },
         {_id: '5cf8efd21c3dc855e58756cf', origin: 13, originAncestors: [13, 12], title: 'بهار ۱۳۹۸'},
     ]);
 };
