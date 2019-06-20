a7e576074c69d3ba726da23d5c699bf2f609650a
morteza <raeisi@zoho.com>
2019-06-20 20:18:54 +0430

    Merge 'get connections' & 'get roles' for user in an era.

--- server/src/api/permissions/get-for-era.js
+++ server/src/api/permissions/get-for-era.js
@@ -1,4 +1,5 @@
 const Joi = require('@hapi/joi');
+const roles = require('../../services/permissions/roles');
 const connections = require('../../services/permissions/connections');
 const CONSTANTS = require('../../core/constants');
 
@@ -11,7 +12,7 @@ const CONSTANTS = require('../../core/constants');
 module.exports = async function (server) {
     server.route({
         method: 'GET',
-        path: 'permissions/connections/{id}/era',
+        path: 'permissions/eras/{id}/{type}',
         handler,
         options: {
             validate
@@ -22,12 +23,21 @@ module.exports = async function (server) {
 const validate = {
     params: Joi.object({
         id: CONSTANTS.joi.objectId(Joi).required(),
+        type: Joi.string().valid(['connections', 'roles']).required(),
     })
 };
 
 const handler = async function (request) {
     const currentUser = request.auth.credentials;
     const eraId = request.params.id;
+    const type = request.params.type;
 
-    return connections.forEra(currentUser._id, eraId);
+    switch (type) {
+        case 'connections':
+            return connections.forEra(currentUser._id, eraId);
+        case 'roles':
+            return roles.forEra(currentUser._id, eraId);
+        default:
+            throw new Error('Type not specified in get permissions.')
+    }
 };
