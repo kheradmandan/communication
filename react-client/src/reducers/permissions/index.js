685d97c97c090f0f63f2af700854ed87aecc2dad
morteza <raeisi@zoho.com>
2019-06-21 22:08:01 +0430

    Convert to map deeply (by fromJs).

--- react-client/src/reducers/permissions/index.js
+++ react-client/src/reducers/permissions/index.js
@@ -1,4 +1,4 @@
-import {Map} from "immutable";
+import {Map, fromJS} from 'immutable';
 import {GET_PERMISSION_FOR_ERA} from "../../constants/permissions";
 
 const initState = Map({});
@@ -6,7 +6,7 @@ const initState = Map({});
 export default function (state = initState, action) {
     switch (action.type) {
         case GET_PERMISSION_FOR_ERA:
-            return state.set(action._id, Map(action.payload));
+            return state.set(action._id, fromJS(action.payload));
 
         default:
             return state;
