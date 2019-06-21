d95f430f66bb32085943752bf995224d88addcd9
morteza <raeisi@zoho.com>
2019-06-21 14:46:05 +0430

    Use new mechanism to get roles. Also, improve query result.

--- server/src/api/issues/get-issue-details.js
+++ server/src/api/issues/get-issue-details.js
@@ -2,7 +2,7 @@ const Joi = require('@hapi/joi');
 const Boom = require('@hapi/boom');
 const Issue = require('../../models/issue');
 const CONSTANTS = require('../../core/constants');
-const {forIssue} = require('../../services/permissions');
+const forIssue = require('../../services/permissions/for-issue');
 
 /**
  * Controller and Validator for [GET issues/{id}] route.
@@ -28,26 +28,31 @@ const validate = {
 };
 
 const handler = async function (request) {
-        const currentUser = request.auth.credentials;
-        const issueId = request.params.id;
+    const currentUser = request.auth.credentials;
+    const issueId = request.params.id;
 
-        // const role = await Issue.getRole(issueId, currentUser._id);
-        const permissions = await forIssue(currentUser._id, issueId);
-        console.log('your permissions are ', permissions);
-
-        if (permissions.length === 0) {
-            throw Boom.badData('Specified issue not found. Maybe you have not enough permission.');
-        }
-
-        const issue = await Issue
-            .findById(issueId)
-            .populate('statuses.created.by', 'name')
-            .populate('priorities.created.by', 'name')
-            .populate('assignees.user', 'name')
-            .populate('assignees.created.by', 'name')
-            .populate('comments.created.by', 'name')
-            .populate('created.by', 'name');
-
-        return {issue, permissions};
+    // fetch roles
+    const permissions = await forIssue(currentUser._id, issueId);
+    if (permissions.length === 0) {
+        throw Boom.badData('Specified issue not found. Maybe you have not enough permission.');
     }
-;
+
+    const issue = await Issue
+        .findById(issueId, 'title sequence realm era statuses priorities assignees comments created')
+        .populate('realm', 'title')
+        .populate('statuses.created.by', 'name')
+        .populate('priorities.created.by', 'name')
+        .populate('assignees.user', 'name')
+        .populate('assignees.created.by', 'name')
+        .populate('comments.created.by', 'name')
+        .populate('created.by', 'name').populate({
+            path: 'era',
+            select: 'title',
+            populate: {
+                path: 'origin',
+                select: 'title'
+            }
+        });
+
+    return {issue, permissions};
+};
