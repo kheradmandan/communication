bccf38e3398658d6d3af46cdec0f71dec78138a1
morteza <raeisi@zoho.com>
2019-06-21 21:27:03 +0430

    Spread values.

--- react-client/src/actions/permissions.js
+++ react-client/src/actions/permissions.js
@@ -3,6 +3,7 @@ import * as permissionConstants from "../constants/permissions";
 export function setPermissionForEra(eraId, payload) {
     return {
         type: permissionConstants.GET_PERMISSION_FOR_ERA,
-        payload: {_id: eraId, payload}
+        _id: eraId,
+        payload
     }
 }
