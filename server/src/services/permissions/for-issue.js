40839b2fa10a65cfe4da44c11a0816b991b0c85f
morteza <raeisi@zoho.com>
2019-06-21 14:25:46 +0430

    Fetch all available roles in issue for specified user.

---
+++ server/src/services/permissions/for-issue.js
@@ -0,0 +1,60 @@
+const Issue = required('../../models/issue');
+const roles = require('./roles');
+
+/**
+ * Fetch all available roles in issue for specified user.
+ * @param userId
+ * @param issueId
+ * @returns {Promise<*[]>}
+ */
+module.exports = async function getPermissionsForIssue(userId, issueId) {
+
+    // fetch issue
+    const issue = await Issue.findById(issueId);
+    if (!issue) {
+        throw new Error('Issue Not Found');
+    }
+    const relation = issue.getRelation(userId);
+
+    // fetch persists
+    const settledRoles = await roles.forEra(userId, issue.era);
+
+    // set defaults
+    if ('CREATOR' === relation) {
+        settledRoles.push(getDefaultOwnerRoles());
+
+    } else if ('ASSIGNEE' === relation) {
+        settledRoles.push(getDefaultAssigneeRoles());
+
+    } else if ('ASSIGNED' === relation) {
+        settledRoles.push(getDefaultAssignedRoles());
+    }
+
+    return [...new Set(settledRoles)]; // unique them
+};
+
+// Just hard code!
+function getDefaultOwnerRoles() {
+    return [
+        'change-issue-priority',
+        'change-issue-assignee',
+        'archive-issue',
+        'remove-issue',
+        'close-issue',
+        'add-comment-to-issue',
+        'add-attachment-to-issue',
+    ]
+}
+
+function getDefaultAssigneeRoles() {
+    return [
+        'change-issue-priority',
+        'change-issue-assignee',
+        'add-comment-to-issue',
+        'add-attachment-to-issue',
+    ]
+}
+
+function getDefaultAssignedRoles() {
+    return []
+}
