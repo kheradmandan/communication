b6e74720bd97aa2ae4493befa654ef2c0836e63e
morteza <raeisi@zoho.com>
2019-06-21 22:06:35 +0430

    Implement <Selection.User />

--- react-client/src/components/Selection/Selection.js
+++ react-client/src/components/Selection/Selection.js
@@ -1,24 +1,5 @@
-import React from 'react';
-import {List} from "immutable";
-import {Dropdown} from "semantic-ui-react";
+import User from './User';
 
-export default function ({options, modelName, onSelection, defaultValue}) {
-
-    if (!options) options = [];
-    if (List.isList(options)) options = options.toJS();
-
-    let mappedOptions = options
-        .filter(x => x[modelName] && x[modelName]['parentId'])
-        .map(x => ({key: x[modelName]['id'], value: x[modelName]['id'], text: x[modelName]['title']}));
-
-    return <Dropdown
-        openOnFocus
-        selection
-        options={mappedOptions}
-        value={defaultValue}
-        name={modelName}
-        onChange={onSelection}
-    />
-
+export default {
+    User
 }
-
