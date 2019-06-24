ef4745709d0a1cb0e9f7242d4d0ca22aeaef4e23
morteza <raeisi@zoho.com>
2019-06-24 22:07:02 +0430

    Integrate all combo items in one place.

---
+++ react-client/src/utils/combo-items.js
@@ -0,0 +1,12 @@
+/**
+ * Assignee titles
+ * @type {*[]}
+ */
+export const assigneeTitlesOptions = [
+    {key: 'ref', icon: 'arrow alternate circle left', text: 'ارجاع', value: 'ارجاع'},
+    {key: 'question', icon: 'question circle', text: 'پرسش', value: 'پرسش'},
+    {key: 'answer', icon: 'aws', text: 'پاسخ', value: 'پاسخ'},
+    {key: 'review', icon: 'video', text: 'مشاهده', value: 'مشاهده'},
+    {key: 'finish', icon: 'calendar check', text: 'پایان', value: 'پایان'},
+    {key: 'again', icon: 'retweet', text: 'دوباره', value: 'دوباره'},
+];
