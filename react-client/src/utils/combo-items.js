bd26a7eed77b3815b520318774b3128315d7a860
morteza <raeisi@zoho.com>
2019-06-24 22:25:03 +0430

    Set rtl arrow direction.

--- react-client/src/utils/combo-items.js
+++ react-client/src/utils/combo-items.js
@@ -3,10 +3,21 @@
  * @type {*[]}
  */
 export const assigneeTitlesOptions = [
-    {key: 'ref', icon: 'arrow alternate circle left', text: 'ارجاع', value: 'ارجاع'},
+    {key: 'ref', icon: 'arrow alternate circle right', text: 'ارجاع', value: 'ارجاع'},
     {key: 'question', icon: 'question circle', text: 'پرسش', value: 'پرسش'},
     {key: 'answer', icon: 'aws', text: 'پاسخ', value: 'پاسخ'},
     {key: 'review', icon: 'video', text: 'مشاهده', value: 'مشاهده'},
     {key: 'finish', icon: 'calendar check', text: 'پایان', value: 'پایان'},
     {key: 'again', icon: 'retweet', text: 'دوباره', value: 'دوباره'},
 ];
+
+/**
+ * Explore in options to find corresponding key
+ * @param value
+ * @param options
+ * @returns {string}
+ */
+export const findKey = function (value, options = []) {
+    const keys = options.filter(x => x.value === value);
+    return keys.length > 0 ? keys[0] : {};
+};
