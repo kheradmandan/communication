42a93a242b9b6f8f6e379523907f0591761c00cd
RaeisiM <raeisi@zoho.com>
2019-06-24 09:04:46 +0430

    Implement 'post,put users/avatar' route to upsert avatar.

---
+++ server/src/api/users/set-avatar.js
@@ -0,0 +1,60 @@
+const Joi = require('@hapi/joi');
+const Attachment = require('../../models/attachment');
+
+/**
+ * Controller and Validator for [POST,PUT users/avatar] route.
+ * @param server
+ * @param option
+ */
+
+module.exports = async function (server, option) {
+    server.route({
+        method: ['POST', 'PUT'],
+        path: 'users/avatar',
+        handler,
+        options: {
+            validate,
+            payload: {
+                maxBytes: 1000 * 300, // 300 Kb
+                output: 'stream',
+                parse: true
+            }
+        }
+    });
+};
+
+const validate = {
+    payload: Joi.object({
+        file: Joi.any().required(),
+        title: Joi.string(),
+    })
+};
+
+const handler = async function (request) {
+    const currentUser = request.auth.credentials;
+    const userId = currentUser._id;
+    const {File, title} = request.payload;
+
+    const data = new Buffer(File._data);
+    const newAttachment = new Attachment({
+        owner: userId,
+        ownerModel: 'User',
+        title,
+        type: File.hapi.headers['content-type'],
+        filename: File.hapi.filename,
+        data,
+        size: data.length,
+        created: {
+            by: currentUser._id,
+            at: new Date()
+        }
+    });
+
+    // create or update avatar
+    await Attachment.update({owner: userId, ownerModel: 'User'}, newAttachment, {
+        upsert: true,
+        setDefaultsOnInsert: true
+    });
+
+    return {Ok: true}
+};
