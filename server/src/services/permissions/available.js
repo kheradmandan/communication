0353b28ed7b52ffd4f791f20f9e860b89c770a2a
morteza <raeisi@zoho.com>
2019-06-25 22:04:27 +0430

    Implement 'get all available permission' module.

---
+++ server/src/services/permissions/available.js
@@ -0,0 +1,61 @@
+const {ObjectId} = require('mongoose').Types;
+const Realm = require('../../models/realm');
+const User = require('../../models/user');
+const Era = require('../../models/era');
+
+/**
+ * Retrieve all available permissions for specified user.
+ * @param userId
+ * @param eraId
+ * @returns {Promise<Array>}
+ */
+module.exports = async function (userId, eraId = null) {
+
+    const matchClause = eraId ?
+        {$match: {_id: ObjectId(eraId)}} : // for one era
+        {$match: {'permissions.user': ObjectId(userId)}}; // in all eras
+
+    let results = await Era
+        .aggregate([
+            matchClause,
+            {$unwind: '$permissions'},
+            {$match: {'permissions.user': ObjectId(userId)}},
+            {
+                $group:
+                    {
+                        _id: '$_id',
+                        origin: {'$first': '$origin'},
+                        title: {'$first': '$title'},
+                        permission: {$push: '$permissions'},
+                    }
+            }
+        ]);
+
+    // not found
+    if (results.length === 0) {
+        return [];
+    }
+
+    // populate
+    results = await User
+        .populate(results, {
+            path: 'permission.connections.user',
+            select: 'name'
+        });
+
+    results = await Realm
+        .populate(results, {
+            path: 'permission.realms.realm',
+            select: 'title'
+        });
+
+    results = await Era
+        .populate(results, {
+            path: 'origin',
+            select: 'name title'
+        });
+
+    // there is one permission at most
+    results.forEach(x => x.permission = x.permission[0]);
+    return eraId ? results[0] : results;
+};
