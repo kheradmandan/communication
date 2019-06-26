b852847a5dbbe6540b88de5ceb04f461a48f1d1c
morteza <raeisi@zoho.com>
2019-06-26 23:36:13 +0430

    Add extra tests and improve grammar.

--- server/src/core/ability/spec.js
+++ server/src/core/ability/spec.js
@@ -4,7 +4,7 @@ const ability = require('.');
 
 describe('core/ability', function abilitySuite() {
 
-    it('Should allow do one thing', () => {
+    it('Should allow to do one thing', () => {
 
         const roles = ['go-shopping', 'have-dinner', 'read-book'];
         const indeed = 'have-dinner';
@@ -12,12 +12,54 @@ describe('core/ability', function abilitySuite() {
         expect(ability.can(roles)(indeed)).to.be.true;
     });
 
-    it('Should allow do multiple thing', () => {
+    it('Should throw to deny to do one thing', () => {
+
+        const roles = ['go-shopping', 'have-dinner', 'read-book'];
+        const indeed = 'have-lunch';
+
+        expect(() => ability.can(roles)(indeed)).to.throw;
+    });
+
+    it('Should deny to do one thing', () => {
+
+        const roles = ['go-shopping', 'have-dinner', 'read-book'];
+        const indeed = 'have-lunch';
+        const throwOnFails = false;
+
+        expect(ability.can(roles)(indeed, throwOnFails)).to.be.false;
+    });
+
+    it('Should allow to do multiple things', () => {
 
         const roles = ['go-shopping', 'have-dinner', 'read-book'];
         const indeed = ['have-dinner', 'go-shopping'];
 
         expect(ability.can(roles)(indeed)).to.be.true;
+    });
+
+    it('Should allow to do extra multiple things', () => {
+
+        const roles = ['go-shopping', 'have-dinner', 'read-book'];
+        const indeed = ['have-lunch', 'go-shopping'];
+
+        expect(ability.can(roles)(indeed)).to.be.true;
+    });
+
+    it('Should throw to deny to do multiple things', () => {
+
+        const roles = ['go-shopping', 'have-dinner', 'read-book'];
+        const indeed = ['have-lunch', 'play-game'];
+
+        expect(() => ability.can(roles)(indeed)).to.throw;
+    });
+
+    it('Should deny to do multiple things', () => {
+
+        const roles = ['go-shopping', 'have-dinner', 'read-book'];
+        const indeed = ['have-lunch', 'play-game'];
+        const throwOnFails = false;
+
+        expect(() => ability.can(roles)(indeed, throwOnFails)).to.throw;
     });
 
 });
