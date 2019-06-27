7c5c255e5797e90af093eea063a6324c1a473bbd
morteza <raeisi@zoho.com>
2019-06-28 00:17:40 +0430

    Refactor and use new api call. That's amazing!

--- react-client/src/services/issues.js
+++ react-client/src/services/issues.js
@@ -1,70 +1,48 @@
-import API from "../utils/API";
-import {remoteUrl} from "../utils/remote-utils";
-import {checkRequestProgress} from "../utils/checkRequestProgress";
-import * as requestTypes from "../constants/request.types";
-import * as issueAction from "../actions/issues";
-import {apiErrorHandler} from "./messages";
+import {getApi, postApi} from '../utils/fetch';
+import * as actions from '../actions/issues';
 
 export const reloadIssues = () => (dispatch, getState) => {
-    const status = checkRequestProgress(requestTypes.ISSUE)(dispatch, getState);
-    if (status.alreadyInProgress) {
-        return;
-    }
 
-    const url = remoteUrl('/issues');
-    API
-        .get(url)
-        .then(({data}) => {
-            dispatch(issueAction.reload(data));
-        })
-        .catch(apiErrorHandler(dispatch, getState))
-        .finally(status.unset())
+    getApi({
+        url: '/issues',
+        title: 'reload-issues',
+        dispatches: [actions.reload],
+        dispatch, getState
+    });
+
 };
 
 export const getIssueDetails = (id) => (dispatch, getState) => {
-    const status = checkRequestProgress(requestTypes.ISSUE)(dispatch, getState);
-    if (status.alreadyInProgress) {
-        return;
-    }
 
-    const url = remoteUrl(`/issues/${id}`);
-    API
-        .get(url)
-        .then(({data}) => {
-            dispatch(issueAction.setCurrentIssueDetails(data));
-        })
-        .catch(apiErrorHandler(dispatch, getState))
-        .finally(status.unset())
+    getApi({
+        url: `/issues/${id}`,
+        title: 'get-issue-details',
+        dispatches: [actions.currentIssue],
+        dispatch, getState
+    });
+
 };
 
 export const addComment = (issueId, context) => (dispatch, getState) => {
-    const status = checkRequestProgress(requestTypes.ISSUE_STATE_CHANGING)(dispatch, getState);
-    if (status.alreadyInProgress) {
-        return;
-    }
 
-    const url = remoteUrl(`/issues/${issueId}/comments`);
-    API
-        .post(url, {context})
-        .then(({data}) => {
-            dispatch(issueAction.setCurrentIssueDetails({id: ''}));
-        })
-        .catch(apiErrorHandler(dispatch, getState))
-        .finally(status.unset())
+    postApi({
+        data: {context},
+        url: `/issues/${issueId}/comments`,
+        title: 'add-comment-to-issue',
+        dispatches: [actions.expireCurrentIssue],
+        dispatch, getState
+    });
+
 };
 
 export const changeAssignee = (issueId, userId, title) => (dispatch, getState) => {
-    const status = checkRequestProgress(requestTypes.ISSUE_ASSIGNEE_CHANGING)(dispatch, getState);
-    if (status.alreadyInProgress) {
-        return;
-    }
 
-    const url = remoteUrl(`/issues/${issueId}/assignees`);
-    API
-        .post(url, {user: userId, title})
-        .then(({data}) => {
-            dispatch(issueAction.assigneeHasChanged(issueId));
-        })
-        .catch(apiErrorHandler(dispatch, getState))
-        .finally(status.unset())
+    postApi({
+        data: {user: userId, title},
+        url: `/issues/${issueId}/assignees`,
+        title: 'change-assignee',
+        dispatches:[actions.assigneeHasChanged],
+        dispatch, getState
+    });
+
 };
