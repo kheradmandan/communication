a95365fc641059e50f2163212a488fe0bc00585f
morteza <raeisi@zoho.com>
2019-06-28 00:50:28 +0430

    Refactor & make meaningful.

--- react-client/src/constants/request.types.js
+++ react-client/src/constants/request.types.js
@@ -1,7 +1,7 @@
 export const AUTHENTICATE_USER = 'authenticate-user-progress';
 
-export const LOAD_ISSUE_LIST = 'load-issue-list-progress';
-export const LOAD_ISSUE_DETAILS = 'load-issue-details-progress';
+export const LOAD_ISSUE_LIST = 'issue-list-progress';
+export const LOAD_ISSUE_DETAILS = 'issue-details-progress';
 export const ADD_COMMENT = 'add-comment-progress';
 export const CHANGE_ASSIGNEE = 'change-assignee-progress';
 export const CHANGE_ISSUE_STATE = 'change-issue-state-progress';
