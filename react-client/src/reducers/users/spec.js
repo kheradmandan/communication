0713f92c285a10b5537de13bfbeebcc904df1508
morteza <raeisi@zoho.com>
2019-06-27 19:59:53 +0430

    Do new conversations.

--- react-client/src/reducers/users/spec.js
+++ react-client/src/reducers/users/spec.js
@@ -1,26 +1,36 @@
 import {describe} from "mocha";
 import {expect} from "chai";
-import reducer from './index';
-import {authFailure, authSuccess} from "../../actions/users";
+import reducer from '.';
+import * as userActions from "../../actions/users";
 
-describe('User Reducer', () => {
+describe('reducers/users', () => {
 
     it('Should store auth info', () => {
+
         const data = {
             type: 'jwt',
             token: 'some-token',
             user: {uuid: 'user uuid', email: 'user@email.com', nickname: 'user nickname'}
         };
 
-        const state = reducer(undefined, authSuccess(data));
-        expect(state.get('session').toJS()).to.have.deep.equals({
-            token: data.type + ' ' + data.token,
+        const state = reducer(undefined, userActions.authSuccess(data));
+        expect(state.get('session').toJS())
+            .to.deep.equals({
+            isSignedIn: true,
+            token: data.token,
             user: data.user
         });
     });
 
     it('Should clear session', () => {
-        const state = reducer(undefined, authFailure('some reason'));
-        expect(state.get('session').toJS()).to.have.deep.equals({user: {}, cause: 'some reason'});
+
+        const state = reducer(undefined, userActions.authFailure('some reason'));
+
+        expect(state.get('session').toJS())
+            .to.deep.equals({
+            isSignedIn: false,
+            user: {},
+            cause: 'some reason'
+        });
     });
 });
