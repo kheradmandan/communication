98e0c5dc8837e90769ccf694afa9d30c1aefd910
morteza <raeisi@zoho.com>
2019-06-28 14:30:04 +0430

    Review and use new components and do conversations.

--- react-client/src/components/IssueDialog/IssueDialog.js
+++ react-client/src/components/IssueDialog/IssueDialog.js
@@ -1,94 +1,98 @@
 import React from 'react';
 import {connect} from "react-redux";
 import {Button, Form, Input, Label, Modal, TextArea} from "semantic-ui-react";
-import Selection from "../Selection";
-import RealmSelection from "../RealmSelection";
+import EraSelection from '../Selection/Era';
+import UserSelection from '../Selection/User';
+import RealmSelection from '../Selection/Realm';
+import PrioritySelection from '../Selection/Priority';
 
 class IssueDialog extends React.Component {
 
     state = {
-        Origin: 0,
-        Realm: 0,
-        Priority: 0,
+        era: '',
+        realm: null,
+        priority: null,
+        assignee: null,
+        title: '',
+        context: '',
     };
 
-    handleSelectionChange = (e, {name, value}) => {
-        const newState = {[name]: value};
-        if (name === 'Origin') {
-            newState.Realm = 0;
-        }
-        this.setState(newState);
+    handleEraChange = (era) => this.setState({era, realm: null, priority: null, assignee: null});
+    handleRealmChange = (realm) => this.setState({realm});
+    handleTitleChange = (e, {value}) => this.setState({title: value});
+    handleContextChange = (e, {value}) => this.setState({context: value});
+    handlePriorityChange = (priority) => this.setState({priority});
+    handleAssigneeChange = (assignee) => this.setState({assignee});
+
+    handleSaveButton = () => {
+
     };
 
     render() {
-        const {permissions} = this.props;
-        const {Origin, Realm, Priority} = this.state;
+        const {era, title, context} = this.state;
 
         return (<Modal trigger={<Button>Add new </Button>} centered={false}>
-            <Modal.Header>
-                اعلام مشکل
-            </Modal.Header>
-            <Modal.Content>
-                <Form>
-                    <Form.Field>
-                        <Label>عنوان*</Label>
-                        <Input
-                            fluid
-                            placeholder="عنوان مختصر*"
-                            icon="bug"
-                        />
-                    </Form.Field>
-                    <Form.Field>
-                        <Label>شرح</Label>
-                        <TextArea placeholder="شرح مسأله"/>
-                    </Form.Field>
-                    <Form.Group>
+                <Modal.Header>
+                    بازکردن مبحث جدید
+                </Modal.Header>
+                <Modal.Content>
+                    <Form>
                         <Form.Field>
-                            <Label>محل</Label>
-                            <Selection defaultValue={Origin}
-                                       options={permissions.get('xref-users-origins')}
-                                       modelName='Origin'
-                                       onSelection={this.handleSelectionChange}
+                            <Label>عنوان*</Label>
+                            <Input
+                                fluid
+                                placeholder="عنوان مختصر*"
+                                icon="bug"
+                                value={title}
+                                onChange={this.handleTitleChange}
                             />
                         </Form.Field>
                         <Form.Field>
-                            <Label>بخش</Label>
-                            <RealmSelection defaultValue={Realm}
-                                            originId={Origin}
-                                            onSelection={this.handleSelectionChange}
+                            <Label>شرح</Label>
+                            <TextArea
+                                value={context}
+                                onChange={this.handleContextChange}
+                                placeholder="بحث"
                             />
                         </Form.Field>
-                        <Form.Field>
-                            <Label>اولویت</Label>
-                            <Selection defaultValue={Priority}
-                                       options={permissions.get('xref-users-origins')}
-                                       modelName='Priority'
-                                       onSelection={this.handleSelectionChange}
-                            />
-                        </Form.Field>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Field>
-                            <Button primary>
-                                ذخیره
-                            </Button>
-                            <Button secondary>
-                                انصراف
-                            </Button>
-                        </Form.Field>
-                    </Form.Group>
-                </Form>
-            </Modal.Content>
+                        <Form.Group>
+                            <Form.Field>
+                                <Label>محل</Label>
+                                <EraSelection defaultValue={era} onChange={this.handleEraChange}/>
+                            </Form.Field>
+                            <Form.Field>
+                                <Label>بخش</Label>
+                                <RealmSelection era={era} onChange={this.handleRealmChange}/>
+                            </Form.Field>
+                            <Form.Field>
 
-        </Modal>)
+                                <Label>گیرنده</Label>
+                                <UserSelection era={era} onChange={this.handleAssigneeChange}/>
+                            </Form.Field>
+                            <Form.Field>
+                                <Label>تقدم</Label>
+                                <PrioritySelection era={era} onChange={this.handlePriorityChange}/>
+                            </Form.Field>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Field>
+                                <Button primary>
+                                    ذخیره
+                                </Button>
+                                <Button secondary>
+                                    انصراف
+                                </Button>
+                            </Form.Field>
+                        </Form.Group>
+                    </Form>
+                </Modal.Content>
+            </Modal>
+        )
     }
 }
 
 function mapStateToProps(state) {
-    return {
-        drafts: state.drafts,
-        permissions: state.permissions,
-    }
+    return {}
 }
 
 export default connect(mapStateToProps)(IssueDialog);
