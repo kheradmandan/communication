6908a7843f64a8cbe265baa3addadc350534278d
morteza <raeisi@zoho.com>
2019-06-29 09:07:48 +0430

    Rebuild and some improvements.

--- react-client/src/components/IssueList/IssueList.js
+++ react-client/src/components/IssueList/IssueList.js
@@ -2,10 +2,11 @@ import React from 'react';
 import propTypes from 'prop-types';
 import {List} from 'immutable';
 import {Link} from 'react-router-dom';
-import {Table} from 'semantic-ui-react';
-
 import User from '../User';
 import LocaleDate from '../LocaleDate';
+import {
+    Button, Table
+} from 'semantic-ui-react';
 
 class IssueList extends React.Component {
     state = {
@@ -18,18 +19,19 @@ class IssueList extends React.Component {
         let {column, direction, data} = this.state;
 
         if (column !== clickedColumn) {
-            data = this.props.issues.sort(cb);
+            data = this.props.issues.sort((r, l) => cb(r) > cb(l));
             this.setState({
                 data,
                 column: clickedColumn,
-                direction: 'ascending'
+                direction: 'up'
             });
             return;
         }
 
         this.setState({
             data: data.reverse(),
-            direction: direction === 'ascending' ? 'descending' : 'ascending'
+            direction: direction === 'down' ? 'up' : 'down'
+            // direction: direction === 'ascending' ? 'descending' : 'ascending'
         })
     };
 
@@ -41,44 +43,70 @@ class IssueList extends React.Component {
         return (<Table compact celled definition selectable color='red' sortable>
             <Table.Header>
                 <Table.Row>
-                    <Table.HeaderCell textAlign='center'
-                                      sorted={column === 'sequence' ? direction : null}
-                                      onClick={this.handleSortClick('sequence', (x) => x.get('sequence'))}>
-                        شماره
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={column === 'title' ? direction : null}
-                                      onClick={this.handleSortClick('title', (x) => x.get('title'))}>
-                        عنوان
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={column === 'realm.title' ? direction : null}
-                                      onClick={this.handleSortClick('realm.title', (x) => x.getIn(['realm', 'title']))}>
-                        بخش
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={column === 'era.origin.title' ? direction : null}
-                                      onClick={this.handleSortClick('era.origin.title', (x) => x.getIn(['era', 'origin', 'title']))}>
-                        محل گزارش
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={column === 'era.title' ? direction : null}
-                                      onClick={this.handleSortClick('era.title', (x) => x.getIn(['era', 'title']))}>
-                        دوره
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={column === 'created.by' ? direction : null}
-                                      onClick={this.handleSortClick('created.by',
-                                          (x) => x.getIn(['created', 'by', 'name', 'first']) + x.getIn(['created', 'by', 'name', 'last']))}>
-                        ایجاد کننده
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={column === 'created.at' ? direction : null}
-                                      onClick={this.handleSortClick('created.at', (x) => x.getIn(['created', 'at']))}>
-                        زمان
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={column === 'priority.id' ? direction : null}
-                                      onClick={this.handleSortClick('priority.id', (x) => x.getIn(['priority', 'id']))}>
-                        اولویت
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={column === 'status.id' ? direction : null}
-                                      onClick={this.handleSortClick('status.id', (x) => x.getIn(['status', 'id']))}>
-                        وضعیت
-                    </Table.HeaderCell>
+                    <MyHeader name='sequence'
+                              path={['sequence']}
+                              icon='sort numeric'
+                              column={column} direction={direction}
+                              onSortClick={this.handleSortClick}
+                              cellProps={{textAlign: 'center'}}
+                              content="شماره"
+                    />
+                    <MyHeader name='title'
+                              path={['title']}
+                              icon='sort alphabet'
+                              column={column} direction={direction}
+                              onSortClick={this.handleSortClick}
+                              content="عنوان"
+                    />
+                    <MyHeader name='realm.title'
+                              path={['realm', 'title']}
+                              icon='sort alphabet'
+                              column={column} direction={direction}
+                              onSortClick={this.handleSortClick}
+                              content="بخش"
+                    />
+                    <MyHeader name='era.origin.title'
+                              path={['era', 'origin', 'title']}
+                              icon='sort alphabet'
+                              column={column} direction={direction}
+                              onSortClick={this.handleSortClick}
+                              content=" محل گزارش"
+                    />
+                    <MyHeader name='era.title'
+                              path={['era', 'title']}
+                              icon='sort alphabet'
+                              column={column} direction={direction}
+                              onSortClick={this.handleSortClick}
+                              content="دوره"
+                    />
+                    <MyHeader name='created.by'
+                              path={['created', 'by', 'name', 'first']}
+                              icon='sort alphabet'
+                              column={column} direction={direction}
+                              onSortClick={this.handleSortClick}
+                              content="ایجاد کننده"
+                    />
+                    <MyHeader name='created.at'
+                              path={['created', 'at']}
+                              icon='sort alphabet'
+                              column={column} direction={direction}
+                              onSortClick={this.handleSortClick}
+                              content="زمان"
+                    />
+                    <MyHeader name='priority.id'
+                              path={['priority', 'id']}
+                              icon='sort numeric'
+                              column={column} direction={direction}
+                              onSortClick={this.handleSortClick}
+                              content="تقدم"
+                    />
+                    <MyHeader name='status.id'
+                              path={['status', 'id']}
+                              icon='sort alphabet'
+                              column={column} direction={direction}
+                              onSortClick={this.handleSortClick}
+                              content="وضعیت"
+                    />
                 </Table.Row>
             </Table.Header>
             <Table.Body>
@@ -91,8 +119,8 @@ class IssueList extends React.Component {
                             <Table.Cell>{issue.getIn(['realm', 'title'])} </Table.Cell>
                             <Table.Cell>{issue.getIn(['era', 'origin', 'title'])} </Table.Cell>
                             <Table.Cell>{issue.getIn(['era', 'title'])} </Table.Cell>
-                            <Table.Cell><User source={issue.getIn(['created', 'by'])}/> </Table.Cell>
-                            <Table.Cell><LocaleDate timestamp={issue.getIn(['created', 'at'])}
+                            <Table.Cell><User source={issue.getIn(['assignees', 0, 'created', 'by'])}/> </Table.Cell>
+                            <Table.Cell><LocaleDate timestamp={issue.getIn(['assignees', 0, 'created', 'at'])}
                                                     relative={true}/></Table.Cell>
                             <Table.Cell>{issue.get('priorities').first().get('id')} </Table.Cell>
                             <Table.Cell>{issue.get('statuses').first().get('id')} </Table.Cell>
@@ -112,3 +140,18 @@ IssueList.defaultProps = {
 };
 
 export default IssueList;
+
+const MyHeader = ({column, direction, name, path = [], icon = 'sort', cellProps, content, onSortClick}) => {
+
+    const iconName = icon + ' ' + (column === name ? direction : '');
+    const onSort = onSortClick(name, issue => issue.getIn(path));
+
+    return <Table.HeaderCell {...cellProps}>
+        <Button.Group size='mini'>
+            <Button icon='filter'/>
+            <Button icon={iconName}
+                    onClick={onSort}/>
+        </Button.Group>
+        {content}
+    </Table.HeaderCell>
+};
