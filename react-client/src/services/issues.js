7fb68fdc477ff9f8601f17800ba7bae52dd0eb49
morteza <raeisi@zoho.com>
2019-06-29 08:00:58 +0430

    Sync for new API CALL interface.

--- react-client/src/services/issues.js
+++ react-client/src/services/issues.js
@@ -8,65 +8,53 @@ import {
     LOAD_ISSUE_LIST
 } from '../constants/request.types';
 
-export const reloadIssues = () => (dispatch, getState) => {
+export const reloadIssues =
+    () =>
+        getApi({
+            url: '/issues',
+            title: LOAD_ISSUE_LIST,
+            dispatches: [actions.reload],
+        });
 
-    getApi({
-        url: '/issues',
-        title: LOAD_ISSUE_LIST,
-        dispatches: [actions.reload],
-        dispatch, getState
-    });
 
-};
+export const getIssueDetails =
+    id =>
+        getApi({
+            url: `/issues/${id}`,
+            title: LOAD_ISSUE_DETAILS,
+            dispatches: [actions.currentIssue],
+        });
 
-export const getIssueDetails = (id) => (dispatch, getState) => {
+export const addComment =
+    (issueId, context) =>
+        postApi({
+            url: `/issues/${issueId}/comments`,
+            data: {context},
+            title: ADD_COMMENT,
+            dispatches: [actions.expireCurrentIssue],
+        });
 
-    getApi({
-        url: `/issues/${id}`,
-        title: LOAD_ISSUE_DETAILS,
-        dispatches: [actions.currentIssue],
-        dispatch, getState
-    });
 
-};
+export const changeAssignee =
+    (issueId, userId, title) =>
+        postApi({
+            url: `/issues/${issueId}/assignees`,
+            data: {user: userId, title},
+            title: CHANGE_ASSIGNEE,
+            dispatches: [actions.assigneeHasChanged],
+        });
 
-export const addComment = (issueId, context) => (dispatch, getState) => {
 
-    postApi({
-        url: `/issues/${issueId}/comments`,
-        data: {context},
-        title: ADD_COMMENT,
-        dispatches: [actions.expireCurrentIssue],
-        dispatch, getState
-    });
+export const updateDraft =
+    payload => dispatch =>
+        dispatch(actions.draftIssue(payload));
 
-};
 
-export const changeAssignee = (issueId, userId, title) => (dispatch, getState) => {
-
-    postApi({
-        url: `/issues/${issueId}/assignees`,
-        data: {user: userId, title},
-        title: CHANGE_ASSIGNEE,
-        dispatches: [actions.assigneeHasChanged],
-        dispatch, getState
-    });
-
-};
-
-export const updateDraft = payload => dispatch => {
-
-    dispatch(actions.draftIssue(payload))
-};
-
-export const addIssue = ({era, realm, priority, title, context}) => (dispatch, getState) => {
-
-    postApi({
-        url: `/issues`,
-        data: {era, realm, priority, title, context},
-        title: ADD_ISSUE,
-        dispatches: [actions.draftIssue],
-        dispatch, getState
-    });
-
-};
+export const addIssue =
+    ({era, realm, priority, title, context}) =>
+        postApi({
+            url: `/issues`,
+            data: {era, realm, priority, title, context},
+            title: ADD_ISSUE,
+            dispatches: [actions.draftIssue],
+        });
