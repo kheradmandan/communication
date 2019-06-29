3986312289123d1f972b40ad60478e13e8ecd2b0
morteza <raeisi@zoho.com>
2019-06-29 08:05:34 +0430

    Sync for new API CALL interface.

--- react-client/src/services/users.js
+++ react-client/src/services/users.js
@@ -1,36 +1,38 @@
-import {postApi} from "../utils/fetch";
-import * as actions from "../actions/users";
-import {AUTHENTICATE_USER} from "../constants/request.types";
-import initializeState from "../utils/initialize-state";
+import {postApi} from '../utils/fetch';
+import * as actions from '../actions/users';
+import initializeState from '../utils/initialize-state';
+import {
+    AUTHENTICATE_USER
+} from "../constants/request.types";
 
-export const auth = (email, password) => (dispatch, getState) => {
+export const auth =
+    (email, password) => (dispatch, getState) =>
+        postApi({
+            data: {email, password},
+            url: '/users/auth',
+            title: AUTHENTICATE_USER,
+            dispatches: [actions.authSuccess],
+            onSuccess: data => {
+                initializeState(dispatch, getState);
+                localStorage.setItem('auth', JSON.stringify(data));
+            },
+            onFailure: () => {
+                dispatch(actions.authFailure('Authentication failed.'));
+            }
+        })(dispatch, getState,);
 
-    postApi({
-        data: {email, password},
-        url: '/users/auth',
-        title: AUTHENTICATE_USER,
-        dispatches: [actions.authSuccess],
-        dispatch, getState,
-        onSuccess: data => {
-            initializeState(dispatch, getState);
-            localStorage.setItem('auth', JSON.stringify(data));
-        },
-        onFailure: () => {
-            dispatch(actions.authFailure('Authentication failed.'));
+
+export const loadPrevSession =
+    () => (dispatch, getState) => {
+
+        const isSignedIn = getState().users.getIn(['session', 'isSignedIn']);
+        if (isSignedIn) {
+            return; // sign in already
         }
-    });
-};
 
-export const loadPrevSession = () => (dispatch, getState) => {
-
-    const isSignedIn = getState().users.getIn(['session', 'isSignedIn']);
-    if (isSignedIn) {
-        return; // sign in already
-    }
-
-    const data = JSON.parse(localStorage.getItem('auth'));
-    if (data && data.user && data.user._id && data.token) {
-        dispatch(actions.authSuccess(data));
-        initializeState(dispatch, getState);
-    }
-};
+        const data = JSON.parse(localStorage.getItem('auth'));
+        if (data && data.user && data.user._id && data.token) {
+            dispatch(actions.authSuccess(data));
+            initializeState(dispatch, getState);
+        }
+    };
