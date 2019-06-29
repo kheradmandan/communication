eaa49b5982b4e9a24ee3022813b36f39744007b3
morteza <raeisi@zoho.com>
2019-06-29 10:06:15 +0430

    Refactor and use <ChangeAssignee/>.

--- react-client/src/components/Issue/AddComment/AddComment.js
+++ react-client/src/components/Issue/AddComment/AddComment.js
@@ -2,87 +2,80 @@ import React from 'react';
 import {Map} from 'immutable';
 import propTypes from 'prop-types';
 import Ability from '../../Ability';
-import Selection from '../../Selection';
-import {assigneeTitlesOptions, findKey} from '../../../utils/combo-items';
+import ChangeAssignee from '../ChangeAssignee';
 import {
     Form,
     Button,
-    Dropdown,
-    Icon
+    TextArea,
 } from 'semantic-ui-react';
-import TextareaAutoSize from 'react-textarea-autosize';
-import ChangeAssignee from '../ChangeAssignee/ChangeAssginee';
+
 
 export default class AddComment extends React.Component {
 
     state = {
         context: '',
-        status: '',
-        title: '',
-        assigneeId: '',
+        assignee: {user: '', title: 'ارجاع'},
+        isAssigneeValid: false,
     };
 
     handleTextChange = (e, {value}) => {
         this.setState({context: value});
     };
 
-    handleUserChange = userId => {
-        this.setState({assigneeId: userId});
-    };
+    handleAssigneeChange = (name, value) => {
 
-    handleAssigneeTitleChanged = (e, {value}) => {
-        this.setState({title: value});
+        const {assignee} = this.state;
+        assignee[name] = value;
+
+        const isAssigneeValid = !!(assignee && assignee.user && assignee.title);
+        this.setState({assignee, isAssigneeValid});
     };
 
     handleSubmit = (e) => {
         e.preventDefault();
-        const {context, assigneeId, title} = this.state;
+        const {context, assignee, isAssigneeValid} = this.state;
         const {issue, onAddComment, onChangeAssignee} = this.props;
 
         if (context && context.length > 0) {
             onAddComment(issue.get('_id'), context)
         }
 
-        if (assigneeId) {
-            onChangeAssignee(issue.get('_id'), assigneeId, title);
+        if (isAssigneeValid) {
+            onChangeAssignee(issue.get('_id'), assignee.user, assignee.title);
         }
     };
 
     render() {
-        const {context, assigneeId, title} = this.state;
-        const {issue,isChangeAssignee} = this.props;
-        const iconName = findKey(assigneeId && title, assigneeTitlesOptions).icon;
+        const {context, assignee, isAssigneeValid} = this.state;
+        const {issue, loading} = this.props;
 
+        const button = <Button loading={loading}
+                               disabled={!context && !isAssigneeValid}
+                               onClick={this.handleSubmit}
+                               content="درج"/>;
         return <Form>
-            <TextareaAutoSize maxLength={2048}
-                              value={context}
-                              useCacheForDOMMeasurements
-                              onChange={this.handleTextChange}
+            <TextArea rows={8}
+                      value={context}
+                      onChange={this.handleTextChange}
             />
 
-            <Ability can='change-issue-assignee' permissions={issue.get('permissions')}>
-                <ChangeAssignee era={issue.getIn(['era'])} issue={issue.get('_id')} loading={isChangeAssignee} onChange={null}/>
-                <Selection.User onChange={this.handleUserChange}/>
-            </Ability>
-            <Button.Group>
-                <Dropdown
-                    className='button icon'
-                    floating
-                    trigger={<React.Fragment/>}
-                    options={assigneeTitlesOptions}
-                    onChange={this.handleAssigneeTitleChanged}
+            <Ability can='change-issue-assignee'
+                     permissions={issue.get('permissions')}
+                     elseControl={button}>
+                <ChangeAssignee
+                    {...assignee}
+                    era={issue.getIn(['era', '_id'])}
+                    onChange={this.handleAssigneeChange}
+                    control={button}
                 />
-                <Button disabled={!context && !assigneeId} onClick={this.handleSubmit}>
-                    <Icon name={iconName}/>
-                    درج
-                </Button>
-            </Button.Group>
+            </Ability>
         </Form>
     }
 }
 
 AddComment.propTypes = {
     issue: propTypes.instanceOf(Map).isRequired,
+    loading: propTypes.bool,
     onAddComment: propTypes.func.isRequired,
     onChangeAssignee: propTypes.func.isRequired,
 };
