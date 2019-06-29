72efb0a69f2740c94fbee89ba14d660bf2c07fba
morteza <raeisi@zoho.com>
2019-06-29 07:58:18 +0430

    Connect to redux store automatically. Now all of api call return a curry that accept a dispatch and getState. It is more naturally to react & redux echo system. Thank you 'Dan'.

--- react-client/src/utils/fetch/index.js
+++ react-client/src/utils/fetch/index.js
@@ -8,42 +8,55 @@ import {checkRequestProgress} from '../checkRequestProgress';
  * @property {String} url destination
  * @property {String} title log purposes and prevent multiple calls
  * @property {Array.<function.<Object>>} dispatches
- * @property {function} dispatch redux store dispatch
- * @property {function} getState redux states
  * @property {Object|undefined} data payload to send [in post requests]
  * @property {function|undefined} onSuccess extra actions on success
  * @property {function|undefined} onFailure extra actions on failure
  */
 
 /**
+ * @typedef {Function} ReduxCaller
+ * @param {function} dispatch redux store dispatch
+ * @param {function} getState redux states
+ * @return {Promise} of axios
+ */
+
+/**
  * Make a HTTP GET request to specified remoteApiEngine
  * @param {Options} options
+ * @return {ReduxCaller}
  */
 export function getApi(options) {
-    fetchApi(produceGetCall(options))(options);
+    return fetchApi(produceGetCall(options))(options);
 }
 
 /**
  * Make a HTTP POST request to specified remoteApiEngine
  * @param {Options} options
+ * @return {ReduxCaller}
  */
 export function postApi(options) {
-    fetchApi(producePostCall(options))(options);
+    return fetchApi(producePostCall(options))(options);
 }
 
 // helpers
 const produceGetCall = ({url}) => remoteApiEngine.get(remoteUrl(url));
 const producePostCall = ({url, data}) => remoteApiEngine.post(remoteUrl(url), data);
 
-// make request
-const fetchApi = apiCall => ({title, dispatches = [], dispatch, getState, onSuccess, onFailure}) => {
+/**
+ * make request
+ * @curried
+ * @function
+ * @param apiCall
+ * @returns {function({title?: *, dispatches?: *, onSuccess?: *, onFailure?: *}):ReduxCaller}
+ */
+const fetchApi = apiCall => ({title, dispatches = [], onSuccess, onFailure}) => (dispatch, getState) => {
 
     const status = checkRequestProgress(title)(dispatch, getState);
     if (status.alreadyInProgress) {
         return;
     }
 
-    apiCall
+    return apiCall
         .then(response => {
             const {data} = response;
             dispatches.forEach(action => dispatch(action(data)));
