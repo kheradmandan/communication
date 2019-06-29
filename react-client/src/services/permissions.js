7d14bd4b05d4ba2a23dbbbeb388f2e8e24d8a137
morteza <raeisi@zoho.com>
2019-06-29 08:02:22 +0430

    Sync for new API CALL interface.

--- react-client/src/services/permissions.js
+++ react-client/src/services/permissions.js
@@ -1,25 +1,23 @@
 import {getApi} from '../utils/fetch';
 import * as actions from '../actions/permissions';
-import {AVAILABLE_PERMISSIONS, FOR_SPECIFIED_ERA_ONLY} from "../constants/request.types";
+import {
+    AVAILABLE_PERMISSIONS,
+    FOR_SPECIFIED_ERA_ONLY
+} from "../constants/request.types";
 
-export const getPermissionForEra = (eraId) => (dispatch, getState) => {
+export const getPermissionForEra =
+    eraId =>
+        getApi({
+            url: `/permissions/eras/${eraId}`,
+            title: FOR_SPECIFIED_ERA_ONLY,
+            dispatches: [data => actions.forSpecifiedEra(eraId, data)],
+        });
 
-    getApi({
-        url: `/permissions/eras/${eraId}`,
-        title: FOR_SPECIFIED_ERA_ONLY,
-        dispatches: [data => actions.forSpecifiedEra(eraId, data)],
-        dispatch, getState
-    });
 
-};
-
-export const getAvailablePermissions = () => (dispatch, getState) => {
-
-    getApi({
-        url: `/permissions/eras/available`,
-        title: AVAILABLE_PERMISSIONS,
-        dispatches: [actions.available],
-        dispatch, getState
-    });
-
-};
+export const getAvailablePermissions =
+    () =>
+        getApi({
+            url: `/permissions/eras/available`,
+            title: AVAILABLE_PERMISSIONS,
+            dispatches: [actions.available],
+        });
