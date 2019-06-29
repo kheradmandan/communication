6b7e7a48ca8afad8d65cf27049134527894f0a3d
morteza <raeisi@zoho.com>
2019-06-29 11:27:23 +0430

    Give meaning to some errors.

--- react-client/src/services/messages.js
+++ react-client/src/services/messages.js
@@ -25,12 +25,31 @@ export const clearMessages = () => (dispatch) => {
     dispatch(actions.clear());
 };
 
-export const apiErrorHandler = (dispatch, getState) => error => {
+export const apiErrorHandler = dispatch => error => {
     console.info(' apiErrorHandler %o', error);
-    if (error.response && error.response.data && error.response.data.error === 'Unauthorized') {
-        localStorage.clear();
-        window.location.href = '/';
-    }
+
     const {config} = error;
-    addMessage(error.message + '\n' + config.url, 'error')(dispatch);
+    let type = 'error';
+    let message = error.message + '\n' + config.url;
+
+    if (error.response) {
+        const {status} = error.response;
+
+        switch (status) {
+            case 403:
+            case 422:
+                type = 'error';
+                message = "منبع درخواستی با وجود ندارد یا شما اجازه دسترسی به آن را ندارید.";
+                break;
+            case 500:
+                type = 'info';
+                message = "سرور خطایی درونی داد.";
+                break;
+            default:
+                type = 'error';
+                message = error.message + '\n' + config.url;
+        }
+    }
+    addMessage(message, type)(dispatch);
+
 };
