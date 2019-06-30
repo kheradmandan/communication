205a708477352b0e2e873fa4bba9ad698bab15ce
morteza <raeisi@zoho.com>
2019-06-30 22:12:09 +0430

    Select by type of ref.

--- server/src/api/issues/get-issue-list.js
+++ server/src/api/issues/get-issue-list.js
@@ -1,6 +1,7 @@
 const Joi = require('@hapi/joi');
 const Boom = require('@hapi/boom');
 const Issue = require('../../models/issue');
+const CONSTANTS = require('../../core/constants').query.issue;
 
 /**
  * Controller and Validator for [GET issues] route.
@@ -22,32 +23,41 @@ module.exports = async function (server, options) {
 const validate = {
     query: Joi.object({
         limit: Joi.number().default(10).max(100),
-        type: Joi.string().default('assignee').valid(['draft', 'mine', 'assignee']),
+        'type[]': Joi.array().items(Joi.string().valid(CONSTANTS.getIssueList)).required(),
     })
 };
 
 const handler = async function (request) {
     const currentUser = request.auth.credentials;
-    const {limit, type} = request.query;
+    const {limit} = request.query;
+    const types = request.query['type[]'] || ['assignee'];
 
-    let criteria = {};
-    switch (type) {
-        case 'draft':
-            criteria = {'created.by': currentUser._id, 'statuses.0.id': 'draft'};
-            break;
-        case 'mine':
-            criteria = {'created.by': currentUser._id, 'statuses.0.id': {$ne: 'draft'}};
-            break;
-        case 'assignee':
-            criteria = {'assignees.0.user': currentUser._id};
-            break;
-
-        default:
-            throw Boom.badRequest(`Type of '${type}' is undefined.`)
-    }
+    let criteria = [];
+    types.forEach(type => {
+        //['created', 'assignee', 'assigned', 'permitted'],
+        switch (type) {
+            case 'draft':
+                criteria.push({'created.by': currentUser._id, 'statuses.0.id': 'draft'});
+                break;
+            case 'created':
+                criteria.push({'created.by': currentUser._id, 'statuses.0.id': {$ne: 'draft'}});
+                break;
+            case 'assignee':
+                criteria.push({'assignees.0.user': currentUser._id});
+                break;
+            case 'assigned':
+                criteria.push({'assignees.user': currentUser._id});
+                break;
+            case 'permitted':
+                //::TODO search by ability
+                break;
+            default:
+                throw Boom.badRequest(`Type of '${type}' is undefined.`)
+        }
+    });
 
     return Issue
-        .find(criteria, {
+        .find({$or: criteria}, {
             assignees: {$slice: 1},
             statuses: {$slice: 1},
             priorities: {$slice: 1}
