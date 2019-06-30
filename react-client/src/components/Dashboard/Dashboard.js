443659bc77209d9f699595232cc815216e76703e
morteza <raeisi@zoho.com>
2019-06-30 21:41:38 +0430

    Query by options.

--- react-client/src/components/Dashboard/Dashboard.js
+++ react-client/src/components/Dashboard/Dashboard.js
@@ -6,21 +6,32 @@ import IssueList from './IssueList';
 import * as issueActions from '../../services/issues';
 import * as requestTypes from '../../constants/request.types';
 import {
-    Button,
+    Button, Dropdown,
     Segment
 } from "semantic-ui-react";
+import {queryOptions} from '../../utils/combo-items';
 
 class Dashboard extends React.Component {
 
+    state = {query: ['created', 'assignee']};
+
+    handleQueryChanged = (e, {value}) => {
+        this.setState({query: value});
+    };
+
     render() {
         const {issues, reloadIssues, isLoading} = this.props;
+        const {query} = this.state;
+        const refreshButtonDisabled = !(query.length);
 
         return (<div>
             <Segment>
-                <Button circular primary icon='refresh'
-                        onClick={reloadIssues} loading={isLoading}/>
-                <IssueList issues={issues}/>
+                <Button circular primary icon='refresh' onClick={reloadIssues} loading={isLoading}
+                        disabled={refreshButtonDisabled}/>
+                <Dropdown options={queryOptions} value={query} onChange={this.handleQueryChanged} placeholder='State'
+                          multiple selection/>
             </Segment>
+            <IssueList issues={issues}/>
         </div>)
     }
 }
